a52ba985697833368a0070ef3236e6e038ea5e8f
Daniel Loreto <279789+loreto@users.noreply.github.com>
2022-08-17 18:03:15 -0700

    [devbox] Prepare code for open-sourcing (#2507)
    ## Summary
    Prepare `devbox` code for open-sourcing by:
    - Adding an initial README (I will improve it over time)
    - Adding a LICENSE and CODE_OF_CONDUCT
    - Adding a copyright notice at the top of each file.
    
    ## How was it tested?
    N/A
    
    ## Is this change backwards-compatible?
    Yes

---
+++ README.md
@@ -0,0 +1,7 @@
+# Devbox: Instant development environments and containers using `nix`.
+
+Devbox is a tool that lets you easily manage development environments.
+
+## Related Work
+
+- [nix](https://nixos.org/)
--- nix/doc.go
+++ nix/doc.go
@@ -1,3 +1,6 @@
+// Copyright 2022 Jetpack Technologies Inc and contributors. All rights reserved.
+// Use of this source code is governed by the license in the LICENSE file.
+
 // Go API for nix.
 // Internally this is a wrapper around the nix command line utilities.
 // I'd love to use a go SDK instead, and drop the dependency on the CLI.
