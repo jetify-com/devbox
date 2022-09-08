50ddff299e50762c28d7561abb8a2670bb968f2a
Daniel Loreto <279789+loreto@users.noreply.github.com>
2022-09-08 10:11:10 -0500

    Improve devbox plan test for go and python

--- devbox_test.go
+++ devbox_test.go
@@ -11,34 +11,49 @@ import (
 	"go.jetpack.io/devbox/planner"
 )
 
-func TestPlan(t *testing.T) {
+func TestDevboxPlan(t *testing.T) {
 	testPaths, err := doublestar.FilepathGlob("./testdata/**/devbox.json")
 	assert.NoError(t, err, "Reading testdata/ should not fail")
-	assert.Greater(t, len(testPaths), 0, "testdata/ should contain at least 1 test")
+
+	examplePaths, err := doublestar.FilepathGlob("./examples/**/devbox.json")
+	assert.NoError(t, err, "Reading examples/ should not fail")
+
+	testPaths = append(testPaths, examplePaths...)
+	assert.Greater(t, len(testPaths), 0, "testdata/ and examples/ should contain at least 1 test")
 
 	for _, testPath := range testPaths {
-		baseDir := filepath.Dir(testPath)
-		t.Run(baseDir, func(t *testing.T) {
-			assert := assert.New(t)
-			goldenFile := filepath.Join(baseDir, "plan.json")
-			hasGoldenFile := fileExists(goldenFile)
-
-			box, err := Open(baseDir)
-			assert.NoErrorf(err, "%s should be a valid devbox project", baseDir)
-			plan := box.Plan()
-			assert.NotEmpty(plan.DevPackages, "the plan should have dev packages")
-			if hasGoldenFile {
-				data, err := os.ReadFile(goldenFile)
-				assert.NoError(err, "plan.json should be readable")
-
-				expected := &planner.Plan{}
-				err = json.Unmarshal(data, &expected)
-				assert.NoError(err, "plan.json should parse correctly")
-
-				assert.Equal(expected, plan)
-			}
-		})
+		testIndividualPlan(t, testPath)
 	}
+}
+
+func testIndividualPlan(t *testing.T, testPath string) {
+	baseDir := filepath.Dir(testPath)
+	t.Run(baseDir, func(t *testing.T) {
+		assert := assert.New(t)
+		goldenFile := filepath.Join(baseDir, "plan.json")
+		hasGoldenFile := fileExists(goldenFile)
+
+		box, err := Open(baseDir)
+		assert.NoErrorf(err, "%s should be a valid devbox project", baseDir)
+		plan := box.Plan()
+
+		if !hasGoldenFile {
+			assert.NotEmpty(plan.DevPackages, "the plan should have dev packages")
+			return
+		}
+
+		data, err := os.ReadFile(goldenFile)
+		assert.NoError(err, "plan.json should be readable")
+
+		expected := &planner.Plan{}
+		err = json.Unmarshal(data, &expected)
+		assert.NoError(err, "plan.json should parse correctly")
+		expected.Errors = nil
+
+		// For now we only compare the DevPackages and RuntimePackages fields:
+		assert.ElementsMatch(expected.DevPackages, plan.DevPackages, "DevPackages should match")
+		assert.ElementsMatch(expected.RuntimePackages, plan.RuntimePackages, "RuntimePackages should match")
+	})
 }
 
 func fileExists(path string) bool {
---
+++ testdata/go/go-1.19/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+)
+
+func main() {
+	expected := "go1.19"
+	goVersion := runtime.Version()
+	fmt.Printf("Go version: %s\n", goVersion)
+	if goVersion != expected {
+		panic(fmt.Errorf("expected version: %s, got: %s", expected, goVersion))
+	}
+}
