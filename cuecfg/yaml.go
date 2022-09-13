14a5aff3fea0403d64b496d8b98e87b658e9183a
savil <676452+savil@users.noreply.github.com>
2022-09-12 19:21:34 -0500

    [easy] make yaml, json, toml specific functions be private in cuecfg (#115)
    ## Summary
    
    These should be private for this package. External callers should invoke
    `cuecfg.Marshal` and `cuecfg.Unmarshal`
    
    ## How was it tested?
    
    compiles

--- cuecfg/yaml.go
+++ cuecfg/yaml.go
@@ -8,10 +8,10 @@ import "gopkg.in/yaml.v3"
 // TODO: consider using cue's YAML marshaller.
 // It might have extra functionality related
 // to the cue language.
-func MarshalYaml(v interface{}) ([]byte, error) {
+func marshalYaml(v interface{}) ([]byte, error) {
 	return yaml.Marshal(v)
 }
 
-func UnmarshalYaml(data []byte, v interface{}) error {
+func unmarshalYaml(data []byte, v interface{}) error {
 	return yaml.Unmarshal(data, v)
 }
