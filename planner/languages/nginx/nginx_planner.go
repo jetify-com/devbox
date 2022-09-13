c6b7e1e449b46bab5dc1c1a90624dfdbcfbdc065
Mike Landau <mikeland86@gmail.com>
2022-09-13 15:07:31 -0700

    Make nginx work on shell

--- planner/languages/nginx/nginx_planner.go
+++ planner/languages/nginx/nginx_planner.go
@@ -4,6 +4,7 @@
 package nginx
 
 import (
+	"fmt"
 	"path/filepath"
 
 	"go.jetpack.io/devbox/planner/plansdk"
@@ -23,20 +24,53 @@ func (p *Planner) IsRelevant(srcDir string) bool {
 }
 
 func (p *Planner) GetPlan(srcDir string) *plansdk.Plan {
+	fmt.Println(srcDir)
 	return &plansdk.Plan{
 		DevPackages: []string{
-			"nginx",
+			"custom-nginx",
+			"shell-nginx",
 		},
 		RuntimePackages: []string{
 			"nginx",
 		},
-		SharedPlan: plansdk.SharedPlan{
-			StartStage: &plansdk.Stage{
-				// These 2 directories are required and are not created by nginx?
-				Command: "mkdir -p /var/cache/nginx/client_body && " +
-					"mkdir -p /var/log/nginx/ && " +
-					"nginx -c /app/nginx.conf -g 'daemon off;'",
-			},
+		InstallStage: &plansdk.Stage{
+			InputFiles: plansdk.AllFiles(),
+		},
+		StartStage: &plansdk.Stage{
+			// These 2 directories are required and are not created by nginx?
+			Command: "mkdir -p /var/cache/nginx/client_body && " +
+				"mkdir -p /var/log/nginx/ && " +
+				"nginx -c /app/nginx.conf -g 'daemon off;'",
+			InputFiles: plansdk.AllFiles(),
+		},
+		// These definitions are only used in dev.
+		Definitions: []string{
+			customNginxDefintion,
+			fmt.Sprintf(nginxShellStartScript, srcDir),
+			// nginxOverwriteDefintion,
 		},
 	}
 }
+
+const customNginxDefintion = `
+custom-nginx = pkgs.nginx.overrideAttrs (oldAttrs: rec {
+	configureFlags = oldAttrs.configureFlags ++ [
+			"--http-client-body-temp-path=/tmp/cache/client_body"
+			"--http-proxy-temp-path=/tmp/cache/proxy"
+			"--http-fastcgi-temp-path=/tmp/cache/fastcgi"
+			"--http-uwsgi-temp-path=/tmp/cache/uwsgi"
+			"--http-scgi-temp-path=/tmp/cache/scgi"
+		];
+});`
+
+const nginxShellStartScript = `
+shell-nginx = pkgs.writeShellScriptBin "shell-nginx" ''
+	echo "Starting nginx with command:"
+	echo "nginx -p %[1]s -c shell-nginx.conf -e /tmp/error.log -g \"pid /tmp/mynginx.pid;daemon off;\""
+  nginx -p %[1]s -c shell-nginx.conf -e /tmp/error.log -g "pid /tmp/mynginx.pid;daemon off;"
+'';`
+
+const nginxOverwriteDefintion = `
+nginx = pkgs.writeScriptBin "nginx" ''
+	exec ${nginxCustom}/bin/nginx -c ${./../../shell-nginx.conf} -e /tmp/error.log -g "pid /tmp/mynginx.pid;daemon off;"
+'';`
--- planner/plansdk/plansdk.go
+++ planner/plansdk/plansdk.go
@@ -43,26 +43,6 @@ type Plan struct {
 	Errors []PlanError `json:"errors,omitempty"`
 }
 
-type Stage struct {
-	Command string `cue:"string" json:"command"`
-	// InputFiles is internal for planners only.
-	InputFiles []string `cue:"[...string]" json:"input_files,omitempty"`
-}
-
-func (s *Stage) GetCommand() string {
-	if s == nil {
-		return ""
-	}
-	return s.Command
-}
-
-func (s *Stage) GetInputFiles() []string {
-	if s == nil {
-		return []string{}
-	}
-	return s.InputFiles
-}
-
 type Planner interface {
 	Name() string
 	IsRelevant(srcDir string) bool
