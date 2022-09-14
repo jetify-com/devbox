60687715722165f76618fe2abe14cd9210ddea42
mohsenari <mohsenari@users.noreply.github.com>
2022-09-14 19:54:06 -0400

    [java planner] part1: shell packages for OpenJDK + Maven (#125)
    ## Summary
    Added shell planner for Java. 
    Includes java versions that are used and available in nix packages.
    (open to expand the list)
    Includes adding maven package by default. This can change later
    depending on which file exists in the project (e.g., `build.gradle`)
    
    ## How was it tested?
    1. build and compile devbox
    ```shell
      go build -o ./devbox cmd/devbox/main.go
      ```
    2. create an empty directory and put the sample `pom.xml` below inside it. (pom files are usually long.)
    3. create an empty devbox.json and check the output of `./devbox plan`
    4. change the version number in `<maven.compiler.source>` to 11 or 8 and verify the output of `./devbox plan` again.
    
    pom file:
    ```xml
    <?xml version="1.0" encoding="UTF-8"?>
    
    <project xmlns="http://maven.apache.org/POM/4.0.0"
    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
    xsi:schemaLocation="http://maven.apache.org/POM/4.0.0
    http://maven.apache.org/xsd/maven-4.0.0.xsd">
      <modelVersion>4.0.0</modelVersion>
    
      <groupId>com.devbox.mavenapp</groupId>
      <artifactId>devbox-maven-app</artifactId>
      <version>1.0-SNAPSHOT</version>
    
      <name>devbox-maven-app</name>
      <!-- FIXME change it to the project's website -->
      <url>http://www.example.com</url>
    
      <properties>
        <project.build.sourceEncoding>UTF-8</project.build.sourceEncoding>
        <maven.compiler.source>17</maven.compiler.source>
        <maven.compiler.target>17</maven.compiler.target>
      </properties>
    
      <dependencies>
        <dependency>
          <groupId>junit</groupId>
          <artifactId>junit</artifactId>
          <version>4.11</version>
          <scope>test</scope>
        </dependency>
      </dependencies>
    
      <build>
    <pluginManagement><!-- lock down plugins versions to avoid using Maven
    defaults (may be moved to parent pom) -->
          <plugins>
    <!-- clean lifecycle, see
    https://maven.apache.org/ref/current/maven-core/lifecycles.html#clean_Lifecycle
    -->
            <plugin>
              <artifactId>maven-clean-plugin</artifactId>
              <version>3.1.0</version>
            </plugin>
    <!-- default lifecycle, jar packaging: see
    https://maven.apache.org/ref/current/maven-core/default-bindings.html#Plugin_bindings_for_jar_packaging
    -->
            <plugin>
              <artifactId>maven-resources-plugin</artifactId>
              <version>3.0.2</version>
            </plugin>
            <plugin>
              <artifactId>maven-compiler-plugin</artifactId>
              <version>3.8.0</version>
            </plugin>
            <plugin>
              <artifactId>maven-surefire-plugin</artifactId>
              <version>2.22.1</version>
            </plugin>
            <plugin>
              <artifactId>maven-jar-plugin</artifactId>
              <version>3.0.2</version>
            </plugin>
            <plugin>
              <artifactId>maven-install-plugin</artifactId>
              <version>2.5.2</version>
            </plugin>
            <plugin>
              <artifactId>maven-deploy-plugin</artifactId>
              <version>2.8.2</version>
            </plugin>
    <!-- site lifecycle, see
    https://maven.apache.org/ref/current/maven-core/lifecycles.html#site_Lifecycle
    -->
            <plugin>
              <artifactId>maven-site-plugin</artifactId>
              <version>3.7.1</version>
            </plugin>
            <plugin>
              <artifactId>maven-project-info-reports-plugin</artifactId>
              <version>3.0.0</version>
            </plugin>
          </plugins>
        </pluginManagement>
      </build>
    </project>
    ```

---
+++ testdata/java/openjdk-17/src/main/java/com/devbox/mavenapp/App.java
@@ -0,0 +1,11 @@
+package com.devbox.mavenapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
