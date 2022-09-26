af0b87476d6139e3e84e9c891319cc13f14ba3a3
mohsenari <mohsenari@users.noreply.github.com>
2022-09-26 19:03:55 -0400

    [Java Planner] part4 - Added support for gradle (#160)
    ## Summary
    Added support for gradle build and run to java planner.
    Only works with Java 17 for now. 
    
    Most of the file changes are due to testdata/java/ (main change in logic
    are in java_planner.go)
    Changes:
    - moved maven test data to testdata/java/maven
    - created a gradle folder at testdata/java/gradle
    - Updated java_planner.go to support detection of build.gradle 
    
    ## How was it tested?
    See instructions in testdata/java/README.md

--- planner/languages/java/java_planner.go
+++ planner/languages/java/java_planner.go
@@ -4,9 +4,11 @@
 package java
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"path/filepath"
-	"strconv"
+	"strings"
 
 	"github.com/creekorful/mvnparser"
 	"github.com/pkg/errors"
@@ -16,17 +18,30 @@ import (
 
 type Planner struct{}
 
-var jVersionMap = map[int]string{
-	8:  "jdk8",
-	11: "jdk11",
-	17: "jdk17_headless",
+// jdk nix packages
+var jVersionMap = map[string]string{
+	"8":  "jdk8",
+	"11": "jdk11",
+	"17": "jdk17_headless",
 }
 
-// "jdk" points to openJDK version 17. OpenJDK v18 is not yet available in nix packages
-const defaultJava = "jdk"
-const defaultMaven = "maven"
+// default nix packages
+const (
+	defaultJava   = "jdk" // "jdk" points to openJDK version 17. OpenJDK v18 is not yet available in nix packages
+	defaultMaven  = "maven"
+	defaultGradle = "gradle"
+)
 
+// misc. nix packages
 const binUtils = "binutils"
+
+// builder tool specific names
+const (
+	MavenType      = "maven"
+	GradleType     = "gradle"
+	mavenFileName  = "pom.xml"
+	gradleFileName = "build.gradle"
+)
 
 // Implements interface Planner (compile-time check)
 var _ plansdk.Planner = (*Planner)(nil)
@@ -36,41 +51,42 @@ func (p *Planner) Name() string {
 }
 
 func (p *Planner) IsRelevant(srcDir string) bool {
-	// Checking for pom.xml (maven) only for now
-	// TODO: add build.gradle file detection
-	pomXMLPath := filepath.Join(srcDir, "pom.xml")
-	return plansdk.FileExists(pomXMLPath)
+	pomXMLPath := filepath.Join(srcDir, mavenFileName)
+	buildGradlePath := filepath.Join(srcDir, gradleFileName)
+	return plansdk.FileExists(pomXMLPath) || plansdk.FileExists(buildGradlePath)
 }
 
 func (p *Planner) GetPlan(srcDir string) *plansdk.Plan {
 	// Creating an empty plan so that we can communicate an error to the user
 	plan := &plansdk.Plan{
-		DevPackages: []string{
-			defaultMaven,
-		},
+		DevPackages: []string{},
 	}
-	javaPkg, err := getJavaPackage(srcDir)
+
+	builderTool, err := p.packageManager(srcDir)
 	if err != nil {
 		return plan.WithError(err)
 	}
-	startCommand, err := p.startCommand(srcDir)
+
+	devPackages, err := p.devPackages(srcDir, builderTool)
 	if err != nil {
 		return plan.WithError(err)
 	}
-	installStage := p.installCommand(srcDir)
+
+	startCommand, err := p.startCommand(srcDir, builderTool)
+	if err != nil {
+		return plan.WithError(err)
+	}
+
+	runtimePackages := p.runtimePackages(builderTool)
+	installCommand := p.installCommand(builderTool)
 	buildCommand := p.buildCommand()
+
 	return &plansdk.Plan{
-		DevPackages: []string{
-			defaultMaven,
-			javaPkg,
-			binUtils,
-		},
-		RuntimePackages: []string{
-			binUtils,
-		},
+		DevPackages:     devPackages,
+		RuntimePackages: runtimePackages,
 		InstallStage: &plansdk.Stage{
 			InputFiles: []string{"."},
-			Command:    installStage,
+			Command:    installCommand,
 		},
 		BuildStage: &plansdk.Stage{
 			InputFiles: []string{"."},
@@ -83,40 +99,87 @@ func (p *Planner) GetPlan(srcDir string) *plansdk.Plan {
 	}
 }
 
+func (p *Planner) packageManager(srcDir string) (string, error) {
+	pomXMLPath := filepath.Join(srcDir, mavenFileName)
+	buildGradlePath := filepath.Join(srcDir, gradleFileName)
+	if plansdk.FileExists(pomXMLPath) {
+		return MavenType, nil
+	} else if plansdk.FileExists(buildGradlePath) {
+		return GradleType, nil
+	} else {
+		return "", errors.New("Could not locate a Maven or Gradle file.")
+	}
+}
+
+func (p *Planner) devPackages(srcDir string, builderTool string) ([]string, error) {
+	javaPkg, err := getJavaPackage(srcDir, builderTool)
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+
+	devPackagesMap := map[string][]string{
+		MavenType: {
+			defaultMaven,
+			javaPkg,
+			binUtils,
+		},
+		GradleType: {
+			defaultGradle,
+			javaPkg,
+			binUtils,
+		},
+	}
+
+	return devPackagesMap[builderTool], nil
+}
+
+func (p *Planner) runtimePackages(builderTool string) []string {
+	return []string{
+		binUtils,
+	}
+}
+
 // This method is added because we plan to differentiate Gradle and Maven.
 // Otherwise, we could just assign the value without calling this.
-func (p *Planner) installCommand(srcDir string) string {
-	// TODO: Add support for Gradle install command
-	return "mvn clean install"
+func (p *Planner) installCommand(builderTool string) string {
+	installCommandMap := map[string]string{
+		MavenType:  "mvn clean install",
+		GradleType: "./gradlew build",
+	}
+	return installCommandMap[builderTool]
 }
 
 func (p *Planner) buildCommand() string {
-	return "jlink --verbose " +
-		"--add-modules ALL-MODULE-PATH " +
-		"--strip-debug" +
-		" --no-man-pages " +
-		" --no-header-files " +
-		" --compress=2 " +
-		"--output ./customjre"
+	return "jlink --verbose" +
+		" --add-modules ALL-MODULE-PATH" +
+		" --strip-debug" +
+		" --no-man-pages" +
+		" --no-header-files" +
+		" --compress=2" +
+		" --output ./customjre"
 }
 
-func (p *Planner) startCommand(srcDir string) (string, error) {
-	pomXMLPath := fmt.Sprintf("%s/pom.xml", srcDir)
-	var parsedPom mvnparser.MavenProject
-	err := cuecfg.ParseFile(pomXMLPath, &parsedPom)
-	if err != nil {
-		return "", errors.WithMessage(err, "error parsing the pom file")
+func (p *Planner) startCommand(srcDir string, builderTool string) (string, error) {
+	if builderTool == MavenType {
+		pomXMLPath := fmt.Sprintf("%s/%s", srcDir, mavenFileName)
+		var parsedPom mvnparser.MavenProject
+		err := cuecfg.ParseFile(pomXMLPath, &parsedPom)
+		if err != nil {
+			return "", errors.WithMessage(err, "error parsing the pom file")
+		}
+		return fmt.Sprintf("./customjre/bin/java -jar target/%s-%s.jar", parsedPom.ArtifactId, parsedPom.Version), nil
+	} else if builderTool == GradleType {
+		return "export JAVA_HOME=./customjre && ./gradlew run", nil
 	}
-	return fmt.Sprintf("./customjre/bin/java -jar target/%s-%s.jar", parsedPom.ArtifactId, parsedPom.Version), nil
+	return "", nil
 }
 
-func getJavaPackage(srcDir string) (string, error) {
-	pomXMLPath := filepath.Join(srcDir, "pom.xml")
-	javaVersion, err := parseJavaVersion(pomXMLPath)
+func getJavaPackage(srcDir string, builderTool string) (string, error) {
+	javaVersion, err := parseJavaVersion(srcDir, builderTool)
 	if err != nil {
 		return "", errors.WithStack(err)
 	}
-	v, ok := jVersionMap[javaVersion]
+	v, ok := jVersionMap[javaVersion.Major()]
 	if ok {
 		return v, nil
 	} else {
@@ -124,21 +187,46 @@ func getJavaPackage(srcDir string) (string, error) {
 	}
 }
 
-func parseJavaVersion(pomXMLPath string) (int, error) {
-	var parsedPom mvnparser.MavenProject
-	// parsing pom.xml and putting its content in 'project'
-	err := cuecfg.ParseFile(pomXMLPath, &parsedPom)
-	if err != nil {
-		return 0, errors.WithMessage(err, "error parsing java version from pom file")
-	}
-	compilerSourceVersion, ok := parsedPom.Properties["maven.compiler.source"]
-	if ok {
-		sourceVersion, err := strconv.Atoi(compilerSourceVersion)
+func parseJavaVersion(srcDir string, builderTool string) (*plansdk.Version, error) {
+	sourceVersion, _ := plansdk.NewVersion("0")
+
+	if builderTool == MavenType {
+		pomXMLPath := filepath.Join(srcDir, mavenFileName)
+		var parsedPom mvnparser.MavenProject
+		// parsing pom.xml and putting its content in 'project'
+		err := cuecfg.ParseFile(pomXMLPath, &parsedPom)
 		if err != nil {
-			return 0, errors.WithMessage(err, "error parsing java version from pom file")
+			return nil, errors.WithMessage(err, "error parsing java version from pom file")
 		}
-		return sourceVersion, nil
+		compilerSourceVersion, ok := parsedPom.Properties["maven.compiler.source"]
+		if ok {
+			sourceVersion, err = plansdk.NewVersion(compilerSourceVersion)
+			if err != nil {
+				return nil, errors.WithMessage(err, "error parsing java version from pom file")
+			}
+		}
+	} else if builderTool == GradleType {
+		buildGradlePath := filepath.Join(srcDir, gradleFileName)
+		readFile, err := os.Open(buildGradlePath)
+		if err != nil {
+			return nil, errors.WithMessage(err, "error parsing java version from gradle file")
+		}
+		fileScanner := bufio.NewScanner(readFile)
+		fileScanner.Split(bufio.ScanLines)
+		// parsing gradle file line by line
+		for fileScanner.Scan() {
+			line := fileScanner.Text()
+			if strings.Contains(line, "sourceCompatibility = ") {
+				compilerSourceVersion := strings.TrimSpace(strings.Split(line, "=")[1])
+				sourceVersion, err = plansdk.NewVersion(compilerSourceVersion)
+				if err != nil {
+					return nil, errors.WithMessage(err, "error parsing java version from gradle file")
+				}
+				break
+			}
+		}
+		readFile.Close()
 	}
 
-	return 0, nil
+	return sourceVersion, nil
 }
--- testdata/java/README.md
+++ testdata/java/README.md
@@ -11,3 +11,27 @@ Maven is an all-in-one CI-CD tool for building testing and deploying Java projec
 4. `mvn package` should compile the package and create a `target/` directory.
 5. `java -cp target/devbox-maven-app-1.0-SNAPSHOT.jar com.devbox.mavenapp.App` should print "Hello World!".
 6. Add `target/` to `.gitignore`.
+
+## Java with Gradle
+To test a sample Gradle app with devbox, follow the steps below:
+
+1. Create a dummy folder: `dummy/` and call `devbox init` inside it. Then add these packages: `devbox add jdk` and `devbox add gradle`.
+    - Replace `jdk` with the version of JDK you want. Get the exact nix-pkg name from `search.nixos.org`.
+2. Then do `devbox shell` to get a shell with that `jdk` nix pkg.
+3. Then do: `gradle init`
+    - In the generated `gradle.build` file, put the following text block:
+        ```gradle
+        apply plugin: 'java'
+        apply plugin: 'application'
+        sourceCompatibility = 17
+        targetCompatibility = 17
+        mainClassName = 'hello.HelloWorld'
+        jar {
+            manifest {
+                attributes 'Main-Class': 'hello.HelloWorld'
+            }
+        }
+        ```
+4. `gradle build` should compile the package and create a `build/` directory that contains an executable jar file.
+5. `gradle run` should print "Hello World!".
+6. Add `build/` to `.gitignore`.
