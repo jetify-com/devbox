e4ebfc818d884923ab3a91fe1ff32ac52873e105
Savil Srivastava <676452+savil@users.noreply.github.com>
2022-10-17 14:09:05 -0700

    [Rust Shell] change to use rustup nix package to avoid overlay

---
+++ testdata/rust/rust-1.62.0/init-shell.sh
@@ -0,0 +1,13 @@
+
+# TODO this only works when devbox shell is started in this directory. Using
+# the --config flag to start the shell will break this.
+# We could inject $JETPACK_CONFIG env-var into the shell environment to replace this.
+projectDir=$(dirname $(readlink -f "$0"))
+echo "project dir is $projectDir"
+
+rustupHomeDir="$projectDir"/.rustup
+mkdir -p $rustupHomeDir
+export RUSTUP_HOME=$rustupHomeDir
+export LIBRARY_PATH=$LIBRARY_PATH:"$projectDir/nix/profile/default/lib"
+
+rustup default 1.62.0
