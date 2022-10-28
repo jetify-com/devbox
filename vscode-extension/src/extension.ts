62dcf803747ff8b6b0ba4f2ef90f4f16e4b81102
Mohsen Ansari <mohsen.ansari@jetpack.io>
2022-10-28 17:54:11 -0400

    Added dockerfile and devcontainer.json file

--- vscode-extension/src/extension.ts
+++ vscode-extension/src/extension.ts
@@ -44,9 +44,23 @@ async function setupDevContainer() {
 			return vscode.window.showInformationMessage('No folder or workspace opened');
 		}
 		const workspaceUri = vscode.workspace.workspaceFolders[0].uri;
+		const devcontainerUri = vscode.Uri.joinPath(workspaceUri, '.devcontainer/');
+		// Parsing devbox.json data
 		const devboxJson = await readDevboxJson(workspaceUri);
-		await vscode.workspace.fs.createDirectory(vscode.Uri.joinPath(workspaceUri, '.devcontainer/'));
-		// console.log(devboxJson);
+		// creating .devcontainer directory and its files
+		await vscode.workspace.fs.createDirectory(devcontainerUri);
+		const dockerfileContent = getDockerfileContent();
+		await vscode.workspace.fs.writeFile(
+			vscode.Uri.joinPath(devcontainerUri, 'Dockerfile'),
+			Buffer.from(dockerfileContent, 'utf8')
+		);
+
+		const devContainerJSON = getDevcontainerJSON();
+		await vscode.workspace.fs.writeFile(
+			vscode.Uri.joinPath(devcontainerUri, 'devcontainer.json'),
+			Buffer.from(devContainerJSON, 'utf8')
+		);
+
 		vscode.window.showInformationMessage(devboxJson["packages"].toString());
 	} catch (error) {
 		console.error('there was an error', error);
@@ -61,6 +75,64 @@ async function readDevboxJson(workspaceUri: vscode.Uri) {
 	const devboxJsonData = JSON.parse(readStr);
 	return devboxJsonData;
 
+}
+
+function getDockerfileContent(): String {
+	return `
+	# See here for image contents: https://github.com/microsoft/vscode-dev-containers/tree/v0.245.2/containers/debian/.devcontainer/base.Dockerfile
+
+	# [Choice] Debian version (use bullseye on local arm64/Apple Silicon): bullseye, buster
+	ARG VARIANT="buster"
+	FROM mcr.microsoft.com/vscode/devcontainers/base:0-\${VARIANT}
+	
+	# These dependencies are required by Nix.
+	RUN apt update -y
+	RUN apt -y install --no-install-recommends curl xz-utils
+	
+	USER vscode
+	
+	# Install nix
+	ARG NIX_INSTALL_SCRIPT=https://nixos.org/nix/install
+	RUN curl -fsSL \${NIX_INSTALL_SCRIPT} | sh -s -- --no-daemon
+	ENV PATH /home/vscode/.nix-profile/bin:\${PATH}
+	
+	# install devbox
+	RUN sudo mkdir /devbox && sudo chown vscode /devbox
+	RUN curl -fsSL https://get.jetpack.io/devbox | FORCE=1 bash
+	COPY --chown=vscode devbox.json /devbox/devbox.json
+	RUN devbox shell --config /devbox/devbox.json -- echo "Nix Store Populated"
+	ENV PATH /devbox/.devbox/nix/profile/default/bin:\${PATH}
+	`;
+}
+
+function getDevcontainerJSON(): String {
+	return `
+	// For format details, see https://aka.ms/devcontainer.json. For config options, see the README at:
+	// https://github.com/microsoft/vscode-dev-containers/tree/v0.245.2/containers/debian
+	{
+		"name": "Debian",
+		"build": {
+			"dockerfile": "./Dockerfile",
+			// Update 'VARIANT' to pick an Debian version: bullseye, buster
+			// Use bullseye on local arm64/Apple Silicon.
+			"args": {
+				"VARIANT": "bullseye"
+			}
+		},
+		"customizations": {
+			"vscode": {
+				"settings": {
+					"python.defaultInterpreterPath": "/devbox/.devbox/nix/profile/default/bin/python3"
+				},
+				"extensions": [
+					"ms-python.python",
+					"golang.go"
+				]
+			}
+		},
+		// Comment out to connect as root instead. More info: https://aka.ms/vscode-remote/containers/non-root.
+		"remoteUser": "vscode"
+	}`;
 }
 
 // This method is called when your extension is deactivated
