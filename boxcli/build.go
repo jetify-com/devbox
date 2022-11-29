cea46f3c8be8689f564f7d36302b10bb7973d639
Mohsen Ansari <mohsen.ansari@jetpack.io>
2022-11-29 15:11:13 -0500

    Made devbox build deprecated

--- boxcli/build.go
+++ boxcli/build.go
@@ -21,12 +21,13 @@ func BuildCmd() *cobra.Command {
 	flags := buildCmdFlags{}
 
 	command := &cobra.Command{
-		Use:   "build",
-		Short: "Build an OCI image that can run as a container",
-		Long:  "Builds your current source directory and devbox configuration as a Docker container. Devbox will create a plan for your container based on your source code, and then apply the packages and stage overrides in your devbox.json. \n To learn more about how to configure your builds, see the [configuration reference](/docs/configuration_reference)",
-		Args:  cobra.MaximumNArgs(1),
+		Use:        "build",
+		Deprecated: "Please follow devbox documentation on how to build a container image around your devbox project.",
+		Short:      "Build an OCI image that can run as a container",
+		Long:       "Builds your current source directory and devbox configuration as a Docker container. Devbox will create a plan for your container based on your source code, and then apply the packages and stage overrides in your devbox.json. \n To learn more about how to configure your builds, see the [configuration reference](/docs/configuration_reference)",
+		Args:       cobra.MaximumNArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			return buildCmdFunc(cmd, args, flags)
+			return nil
 		},
 	}
 
