d4ab6d697032556c9d1027dc5b10d654be537b0c
Mohsen Ansari <mohsen.ansari@jetpack.io>
2022-12-21 13:45:10 -0500

    Added suggestions and remove planner for python

--- internal/pkgsuggest/suggestors/python/python_poetry.go
+++ internal/pkgsuggest/suggestors/python/python_poetry.go
@@ -1,6 +1,3 @@
-// Copyright 2022 Jetpack Technologies Inc and contributors. All rights reserved.
-// Use of this source code is governed by the license in the LICENSE file.
-
 package python
 
 import (
@@ -9,39 +6,33 @@ import (
 	"path/filepath"
 
 	"github.com/pelletier/go-toml/v2"
+	"go.jetpack.io/devbox/internal/pkgsuggest/suggestors"
 	"go.jetpack.io/devbox/internal/planner/plansdk"
 )
 
-type PoetryPlanner struct{}
+type SuggestorPoetry struct{}
 
-// PythonPoetryPlanner implements interface Planner (compile-time check)
-var _ plansdk.Planner = (*PoetryPlanner)(nil)
+// implements interface Suggestor (compile-time check)
+var _ suggestors.Suggestor = (*SuggestorPoetry)(nil)
 
-func (p *PoetryPlanner) Name() string {
-	return "python.Planner"
-}
-
-func (p *PoetryPlanner) IsRelevant(srcDir string) bool {
+func (s *SuggestorPoetry) IsRelevant(srcDir string) bool {
 	return plansdk.FileExists(filepath.Join(srcDir, "poetry.lock")) ||
 		plansdk.FileExists(filepath.Join(srcDir, "pyproject.toml"))
 }
-
-func (p *PoetryPlanner) GetShellPlan(srcDir string) *plansdk.ShellPlan {
-	version := p.PythonVersion(srcDir)
+func (s *SuggestorPoetry) Packages(srcDir string) []string {
+	version := s.PythonVersion(srcDir)
 	pythonPkg := fmt.Sprintf("python%s", version.MajorMinorConcatenated())
 
-	return &plansdk.ShellPlan{
-		DevPackages: []string{
-			pythonPkg,
-			"poetry",
-		},
+	return []string{
+		pythonPkg,
+		"poetry",
 	}
 }
 
 // TODO: This can be generalized to all python planners
-func (p *PoetryPlanner) PythonVersion(srcDir string) *plansdk.Version {
+func (s *SuggestorPoetry) PythonVersion(srcDir string) *plansdk.Version {
 	defaultVersion, _ := plansdk.NewVersion("3.10.6")
-	project := p.PyProject(srcDir)
+	project := s.PyProject(srcDir)
 
 	if project == nil {
 		return defaultVersion
@@ -69,7 +60,7 @@ type pyProject struct {
 	} `toml:"tool"`
 }
 
-func (p *PoetryPlanner) PyProject(srcDir string) *pyProject {
+func (s *SuggestorPoetry) PyProject(srcDir string) *pyProject {
 	pyProjectPath := filepath.Join(srcDir, "pyproject.toml")
 	content, err := os.ReadFile(pyProjectPath)
 	if err != nil {
