7e270643d66884f32a6b991ef6b51d24127460e0
Mohsen Ansari <mohsen.ansari@jetpack.io>
2023-01-03 17:55:35 -0500

    Added -q to devbox init

--- internal/boxcli/init.go
+++ internal/boxcli/init.go
@@ -10,21 +10,28 @@ import (
 	"go.jetpack.io/devbox/internal/boxcli/writer"
 )
 
+type initCmdFlags struct {
+	quiet bool
+}
+
 func InitCmd() *cobra.Command {
+	flags := initCmdFlags{}
 	command := &cobra.Command{
 		Use:   "init [<dir>]",
 		Short: "Initialize a directory as a devbox project",
 		Long:  "Initialize a directory as a devbox project. This will create an empty devbox.json in the current directory. You can then add packages using `devbox add`",
 		Args:  cobra.MaximumNArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			return runInitCmd(cmd, args)
+			return runInitCmd(cmd, args, flags)
 		},
 	}
+	command.Flags().BoolVarP(
+		&flags.quiet, "quiet", "q", false, "Quiet mode: Suppresses logs.")
 
 	return command
 }
 
-func runInitCmd(cmd *cobra.Command, args []string) error {
+func runInitCmd(cmd *cobra.Command, args []string, flags initCmdFlags) error {
 	path := pathArg(args)
 
 	w := writer.New(cmd)
