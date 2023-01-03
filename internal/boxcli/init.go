21fe359c1b076ec3d28f687966b4d6cd7f5d6397
Mohsen Ansari <mohsen.ansari@jetpack.io>
2023-01-03 17:55:35 -0500

    Added -q to devbox init

--- internal/boxcli/init.go
+++ internal/boxcli/init.go
@@ -7,26 +7,35 @@ import (
 	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 	"go.jetpack.io/devbox"
+	"go.jetpack.io/devbox/internal/boxcli/writer"
 )
 
+type initCmdFlags struct {
+	quiet bool
+}
+
 func InitCmd() *cobra.Command {
+	flags := initCmdFlags{}
 	command := &cobra.Command{
 		Use:   "init [<dir>]",
 		Short: "Initialize a directory as a devbox project",
 		Long:  "Initialize a directory as a devbox project. This will create an empty devbox.json in the current directory. You can then add packages using `devbox add`",
 		Args:  cobra.MaximumNArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			return runInitCmd(cmd, args)
+			return runInitCmd(cmd, args, flags)
 		},
 	}
+	command.Flags().BoolVarP(
+		&flags.quiet, "quiet", "q", false, "Quiet mode: Suppresses logs.")
 
 	return command
 }
 
-func runInitCmd(cmd *cobra.Command, args []string) error {
+func runInitCmd(cmd *cobra.Command, args []string, flags initCmdFlags) error {
 	path := pathArg(args)
 
-	_, err := devbox.InitConfig(path, cmd.OutOrStderr())
+	w := &writer.DevboxIOWriter{W: cmd.OutOrStderr(), Quiet: flags.quiet}
+	_, err := devbox.InitConfig(path, w)
 	if err != nil {
 		return errors.WithStack(err)
 	}
