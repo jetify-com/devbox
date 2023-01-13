f2b181bc490d279e4ae610bbf878dd19c586745e
Mike Landau <mikeland86@gmail.com>
2023-01-13 15:05:14 -0800

    [nix] Improve nix install

--- internal/nix/install.go
+++ internal/nix/install.go
@@ -1,24 +1,39 @@
 package nix
 
 import (
+	"bytes"
 	_ "embed"
 	"fmt"
 	"io"
 	"os"
 	"os/exec"
+	"strings"
 
+	"github.com/fatih/color"
 	"github.com/pkg/errors"
 )
 
-//go:embed install.sh
-var installScript string
+const rootError = "warning: installing Nix as root is not supported by this script!"
 
-func Install(writer io.Writer) error {
+// Install runs the install script for Nix. daemon has 3 states
+// nil is unset. false is --no-daemon. true is --daemon.
+func Install(writer io.Writer, daemon *bool) error {
 	r, w, err := os.Pipe()
 	if err != nil {
 		return errors.WithStack(err)
 	}
 	defer r.Close()
+
+	installScript := "curl -L https://nixos.org/nix/install | sh -s"
+	if daemon != nil {
+		if *daemon {
+			installScript += " -- --daemon"
+		} else {
+			installScript += " -- --no-daemon"
+		}
+	}
+
+	fmt.Fprintf(writer, "Installing nix with: %s\nThis may require sudo access.\n", installScript)
 
 	cmd := exec.Command("sh", "-c", installScript)
 	// Attach stdout but no stdin. This makes the command run in non-TTY mode
@@ -28,7 +43,6 @@ func Install(writer io.Writer) error {
 	cmd.Stdout = w
 	cmd.Stderr = w
 
-	fmt.Fprintln(writer, "Installing Nix. This may require sudo access.")
 	err = cmd.Start()
 	w.Close()
 	if err != nil {
@@ -37,9 +51,17 @@ func Install(writer io.Writer) error {
 
 	done := make(chan struct{})
 	go func() {
-		_, err := io.Copy(os.Stdout, r)
+		var buf bytes.Buffer
+		_, err := io.Copy(io.MultiWriter(&buf, os.Stdout), r)
 		if err != nil {
 			fmt.Fprintln(writer, err)
+		}
+
+		if strings.Contains(buf.String(), rootError) {
+			color.New(color.FgYellow).Fprintln(
+				writer,
+				"If installing nix as root, consider using the --daemon flag to install in multi-user mode.",
+			)
 		}
 		close(done)
 	}()
