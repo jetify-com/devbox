5b839d2d73e4fe451e4b8bbea098e21699df680f
mohsenari <mohsen@mailbox.org>
2023-01-19 17:46:44 -0500

    more cleanup and copyright text

--- examples/testdata/commands/info/info_test.go
+++ examples/testdata/commands/info/info_test.go
@@ -1,7 +1,8 @@
+// Copyright 2023 Jetpack Technologies Inc and contributors. All rights reserved.
+// Use of this source code is governed by the license in the LICENSE file.
 package testinfo
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -13,5 +14,4 @@ func TestInfo(t *testing.T) {
 	output, err := td.Info("notarealpackage", false)
 	assert.NoError(t, err)
 	assert.Contains(t, output, "Package notarealpackage not found")
-	fmt.Println(output + "testsetsts")
 }
