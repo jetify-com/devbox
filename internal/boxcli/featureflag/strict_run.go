d4cdfe740afe2ffb7be442717a56bb9603522bdf
Rodrigo Ipince <rodrigo@jetpack.io>
2023-01-25 16:55:38 -0800

    Unflip strict run feature flag (#519)
    ## Summary
    
    Turning the feature off after finding some issues when running against
    `devbox-examples`.

--- internal/boxcli/featureflag/strict_run.go
+++ internal/boxcli/featureflag/strict_run.go
@@ -4,4 +4,4 @@ package featureflag
 // runs the script in the environment returned by `nix print-dev-env`. This means the
 // environment is much more "strict" or "pure", since it will _not_ include parts of
 // the host's environment like `devbox shell` does.
-var StrictRun = enabled("STRICT_RUN")
+var StrictRun = disabled("STRICT_RUN")
