7e54424c9248e2536287b3b4a62add5844568672
mohsenari <mohsen@mailbox.org>
2023-02-02 16:35:30 -0500

    Added sample shellspec test

---
+++ testscripts/shellspec/spec/devbox_spec.sh
@@ -0,0 +1,28 @@
+Describe 'devbox init tests'
+    AfterAll '$(rm devbox.json && rm -r .devbox)'
+    Path devboxJSON="devbox.json"
+    It 'Checks devbox vesion'
+        When call devbox version
+        The output should equal '0.3.2'
+    End
+    It 'Creates a devbox.json'
+        When run devbox init
+        The path devboxJSON should be file
+        The path devboxJSON should not be empty file
+    End
+    It 'Adds a package'
+        When run devbox add hello
+        The stderr should include 'hello (hello-2.12.1) is now installed.'
+    End
+    Before 'export DEVBOX_FEATURE_STRICT_RUN=1'
+    It 'Runs Hello'
+        When run devbox run hello
+        The stderr should include 'Ensuring packages are installed.'
+        The status should equal '0'
+        The stdout should equal 'Hello, world!'
+    End
+    It 'Removes package'
+        When run devbox rm hello
+        The stderr should include 'hello (hello-2.12.1) is now removed.'
+    End
+End
