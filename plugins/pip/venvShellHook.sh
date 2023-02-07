d168fd15e7664d943908d6249f55bd5b082c607f
John Lago <750845+Lagoja@users.noreply.github.com>
2023-02-07 13:17:05 -0800

    cleanup script pt2

--- plugins/pip/venvShellHook.sh
+++ plugins/pip/venvShellHook.sh
@@ -4,8 +4,6 @@ if [ -d "$VENV_DIR" ]; then
     echo "Skipping venv creation, '${VENV_DIR}' already exists"
 else
     echo "Creating new venv environment in path: '${VENV_DIR}'"
-    # Note that the module venv was only introduced in python 3, so for 2.7
-    # this needs to be replaced with a call to virtualenv
     python3 -m venv "$VENV_DIR"
 fi
 echo "You an activate the virtual environment by running 'source $VENV_DIR/bin/activate'"
