d39e0dd4f3ab3940c6d7239f5233e1294fc9705f
Mike Landau <mikeland86@gmail.com>
2023-02-08 15:57:01 -0800

    Bad merge 2

--- internal/nix/nix.go
+++ internal/nix/nix.go
@@ -4,7 +4,6 @@
 package nix
 
 import (
-	"bytes"
 	"encoding/json"
 	"fmt"
 	"os"
@@ -162,37 +161,4 @@ func PrintDevEnv(nixShellFilePath, nixFlakesFilePath string) (*varsAndFuncs, err
 		return nil, errors.WithStack(err)
 	}
 	return &vaf, nil
-}
-
-// ProfileInstall calls nix profile install with default profile
-func ProfileInstall(nixpkgsCommit, pkg string) error {
-	cmd := exec.Command("nix", "profile", "install",
-		"nixpkgs/"+nixpkgsCommit+"#"+pkg,
-		"--extra-experimental-features", "nix-command flakes",
-	)
-	cmd.Env = DefaultEnv()
-	out, err := cmd.CombinedOutput()
-	if bytes.Contains(out, []byte("does not provide attribute")) {
-		return ErrPackageNotFound
-	}
-
-	return errors.WithStack(err)
-}
-
-func ProfileRemove(nixpkgsCommit, pkg string) error {
-	info, found := flakesPkgInfo(nixpkgsCommit, pkg)
-	if !found {
-		return ErrPackageNotFound
-	}
-	cmd := exec.Command("nix", "profile", "remove",
-		info.attributeKey,
-		"--extra-experimental-features", "nix-command flakes",
-	)
-	cmd.Env = DefaultEnv()
-	out, err := cmd.CombinedOutput()
-	if bytes.Contains(out, []byte("does not match any packages")) {
-		return ErrPackageNotInstalled
-	}
-
-	return errors.WithStack(err)
 }
