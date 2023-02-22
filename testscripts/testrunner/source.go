87d33c98701944b95f8cce50076ff43913327dd9
Mike Landau <mikeland86@gmail.com>
2023-02-22 15:05:31 -0800

    Fix lint issue

--- testscripts/testrunner/source.go
+++ testscripts/testrunner/source.go
@@ -14,6 +14,9 @@ func sourcePath(script *testscript.TestScript, neg bool, args []string) {
 	if len(args) != 0 {
 		script.Fatalf("usage: source.path")
 	}
+	if neg {
+		script.Fatalf("source.path does not support negation")
+	}
 	sourcedScript := script.ReadFile("stdout")
 	for _, line := range strings.Split(sourcedScript, "\n") {
 		if strings.HasPrefix(line, "export PATH=") {
