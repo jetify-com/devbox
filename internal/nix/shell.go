d6f30e34de6491c667ff97bea709861697e8a5fb
Rodrigo Ipince <rodrigo@jetpack.io>
2023-03-06 14:34:22 -0500

    Fix fish shell after #706 (#721)
    ## Summary
    * Applies the same changes from #706 to the fish shellrc.
    * Ignores the `_` variable. It's read-only and `fish` complains if we
    try to write it.
    * Added a note in `shellrc` so we remember do edit `shellrc_fish` too.
    If we had tests for fish we could remove this silly note.
    * Alphabetized vars so the generated shellrc is a bit cleaner (imo).
    
    Fixes #719 
    
    ## How was it tested?
    ```
    SHELL=fish ./devbox shell
    ```
    
    I'll add some shellrc_fish tests in a future PR

--- internal/nix/shell.go
+++ internal/nix/shell.go
@@ -12,6 +12,7 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"sort"
 	"strings"
 	"text/template"
 
@@ -217,8 +218,14 @@ func WithHistoryFile(historyFile string) ShellOption {
 // via wrapper scripts.
 func WithEnvVariables(envVariables map[string]string) ShellOption {
 	return func(s *DevboxShell) {
-		for k, v := range envVariables {
-			s.env = append(s.env, fmt.Sprintf("%s=%s", k, v))
+		keys := make([]string, 0, len(envVariables))
+		for k := range envVariables {
+			keys = append(keys, k)
+		}
+		sort.Strings(keys)
+
+		for _, k := range keys {
+			s.env = append(s.env, fmt.Sprintf("%s=%s", k, envVariables[k]))
 		}
 	}
 }
