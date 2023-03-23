491fcfa6c71a52cf575d4c095902ed3e4c09ea1b
savil <676452+savil@users.noreply.github.com>
2023-03-23 16:06:35 -0700

    Fix typo in pip plugin (#810)
    ## Summary
    
    ## How was it tested?
    
    Signed-off-by: savil <676452+savil@users.noreply.github.com>

--- plugins/pip/venvShellHook.sh
+++ plugins/pip/venvShellHook.sh
@@ -6,4 +6,4 @@ else
     echo "Creating new venv environment in path: '${VENV_DIR}'"
     python3 -m venv "$VENV_DIR"
 fi
-echo "You an activate the virtual environment by running 'source \$VENV_DIR/bin/activate'"
+echo "You can activate the virtual environment by running 'source \$VENV_DIR/bin/activate'"
