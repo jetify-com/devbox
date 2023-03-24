0a720374bb0f52addf952f559d661c8fb153d609
Savil Srivastava <676452+savil@users.noreply.github.com>
2023-03-24 12:17:20 -0700

    [lagobot] run tests on DEVBOX_EXAMPLE_TESTS env-var, instead of build tag

--- testscripts/testscripts_test.go
+++ testscripts/testscripts_test.go
@@ -2,10 +2,13 @@ package testrunner
 
 import (
 	"os"
+	"strconv"
 	"testing"
 
 	"go.jetpack.io/devbox/testscripts/testrunner"
 )
+
+const exampleTestsEnvName = "DEVBOX_EXAMPLE_TESTS"
 
 func TestScripts(t *testing.T) {
 	testrunner.RunTestscripts(t, ".")
@@ -13,4 +16,14 @@ func TestScripts(t *testing.T) {
 
 func TestMain(m *testing.M) {
 	os.Exit(testrunner.Main(m))
+}
+
+// TestExamples runs testscripts on the devbox-projects in the examples folder.
+func TestExamples(t *testing.T) {
+	isOn, err := strconv.ParseBool(os.Getenv(exampleTestsEnvName))
+	if err != nil || !isOn {
+		t.Skipf("Skipping TestExamples. To enable, set %s=1.", exampleTestsEnvName)
+	}
+
+	testrunner.RunExamplesTestscripts(t, "../examples")
 }
