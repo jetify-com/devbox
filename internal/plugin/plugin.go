dd54298063a6ce6648a845e93c2f81cfa667a8de
Mike Landau <mikeland86@gmail.com>
2023-03-31 10:51:42 -0700

    [plugins] Always create plugin dir (#858)
    ## Summary
    
    We were not creating the plugin dir if there were no files in it but the
    redis plugin depends on the directory existing. Solution is to always
    create directory.
    
    Fixes https://github.com/jetpack-io/devbox/issues/856
    
    ## How was it tested?
    
    ```
    devbox add redis
    devbox services start redis
    ```

--- internal/plugin/plugin.go
+++ internal/plugin/plugin.go
@@ -49,6 +49,11 @@ func (m *Manager) CreateFilesAndShowReadme(pkg, projectDir string) error {
 		return nil
 	}
 
+	// Always create this dir because some plugins depend on it.
+	if err = createDir(filepath.Join(projectDir, VirtenvPath, pkg)); err != nil {
+		return err
+	}
+
 	debug.Log("Creating files for package %q create files", pkg)
 	for filePath, contentPath := range cfg.CreateFiles {
 		if !m.shouldCreateFile(filePath) {
