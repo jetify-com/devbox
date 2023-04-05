a7cd450032b612135010a3ad4ee29909bd77f6da
Lucille Hua <lucille.hua@gmail.com>
2023-04-05 12:08:40 -0700

    Skip interactive survey if devbox is not executed in a terminal (#867)
    ## Summary
    Skip interactive survey if devbox is not executed in a terminal. When
    `devbox init` or `devbox generate direnv` is called in a dockerfile for
    example, we do not want to prompt for user input.
    
    I tried `echo 'y' | devbox generate direnv` or `yes | devbox generate
    direnv` or `devbox generate direnv <<< "y"`. None of them worked.
    
    Bug in the survey package:
    https://github.com/go-survey/survey/issues/394
    
    ## How was it tested?
    devbox run build
    ./dist/devbox init (interactive mode)
    
    docker build -t devbox-cli .
    docker run --entrypoint=/bin/sh -ti devbox-cli (non interactive mode)

--- internal/impl/devbox.go
+++ internal/impl/devbox.go
@@ -17,6 +17,7 @@ import (
 
 	"github.com/AlecAivazis/survey/v2"
 	"github.com/fatih/color"
+	"github.com/mattn/go-isatty"
 	"github.com/pkg/errors"
 	"github.com/samber/lo"
 
@@ -363,15 +364,18 @@ func (d *Devbox) GenerateEnvrc(force bool, source string) error {
 	if commandExists("direnv") {
 		// prompt for direnv allow
 		var result string
-		prompt := &survey.Input{
-			Message: "Do you want to enable direnv integration for this devbox project? [y/N]",
-		}
-		err := survey.AskOne(prompt, &result)
-		if err != nil {
-			return errors.WithStack(err)
+		isInteractiveMode := isatty.IsTerminal(os.Stdin.Fd())
+		if isInteractiveMode {
+			prompt := &survey.Input{
+				Message: "Do you want to enable direnv integration for this devbox project? [y/N]",
+			}
+			err := survey.AskOne(prompt, &result)
+			if err != nil {
+				return errors.WithStack(err)
+			}
 		}
 
-		if strings.ToLower(result) == "y" {
+		if strings.ToLower(result) == "y" || !isInteractiveMode {
 			// .envrc file creation
 			err := generate.CreateEnvrc(tmplFS, d.projectDir)
 			if err != nil {
