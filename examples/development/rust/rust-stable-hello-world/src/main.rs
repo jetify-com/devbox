3d43a840f9b54d85e381f89d4b849c63cf831ef3
Savil Srivastava <676452+savil@users.noreply.github.com>
2023-04-10 16:34:03 -0700

    simplify rust example

--- examples/development/rust/rust-stable-hello-world/src/main.rs
+++ examples/development/rust/rust-stable-hello-world/src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("Hello, world!");
+  println!("{}", hello_world())
+}
+
+fn hello_world() -> &'static str {
+    return "Hello, world!";
+}
+
+mod tests {
+  use super::*;
+
+  #[test]
+  fn test_hello_world() {
+    assert_eq!(hello_world(), "Hello, world!")
+  }
 }
