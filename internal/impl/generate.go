7c77b1ec33eecd23e96c537eb2344975d3e1cab9
Savil Srivastava <676452+savil@users.noreply.github.com>
2023-04-12 14:44:21 -0700

    [direnv] drop shell.nix, rely solely on devbox shell --print-env

--- internal/impl/generate.go
+++ internal/impl/generate.go
@@ -24,22 +24,11 @@ import (
 //go:embed tmpl/*
 var tmplFS embed.FS
 
-var shellFiles = []string{"shell.nix"}
-
 func (d *Devbox) generateShellFiles() error {
 
 	plan, err := d.ShellPlan()
 	if err != nil {
 		return err
-	}
-
-	outPath := filepath.Join(d.projectDir, ".devbox/gen")
-
-	for _, file := range shellFiles {
-		err := writeFromTemplate(outPath, plan, file)
-		if err != nil {
-			return errors.WithStack(err)
-		}
 	}
 
 	// Gitignore file is added to the .devbox directory
@@ -48,6 +37,7 @@ func (d *Devbox) generateShellFiles() error {
 		return errors.WithStack(err)
 	}
 
+	outPath := filepath.Join(d.projectDir, ".devbox/gen")
 	err = makeFlakeFile(outPath, plan)
 	if err != nil {
 		return errors.WithStack(err)
