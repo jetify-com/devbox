d06b318987c6382e125b0a15ed3cdae502ad6d78
John Lago <750845+Lagoja@users.noreply.github.com>
2023-04-18 11:58:42 -0700

    comment the flake.nix

--- examples/flakes/php-extension/my-php-flake/flake.nix
+++ examples/flakes/php-extension/my-php-flake/flake.nix
@@ -12,7 +12,10 @@
       let pkgs = nixpkgs.legacyPackages.${system};
       in {
         packages = {
+          # Customize and export the PHP package with some extra config
           php = pkgs.php.buildEnv {
+            # extraConfig will add the line below to our php.ini
+            # ${self} is a variable representing the current flake
             extraConfig = ''
               extension=${self}/skeleton.so 
             '';
