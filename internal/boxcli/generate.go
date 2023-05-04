007918f93a4dbefeecc28d47b25c4ab2bea007c0
mohsenari <mohsen@mailbox.org>
2023-05-04 12:18:06 -0400

    change flag to --envrc-fn

--- internal/boxcli/generate.go
+++ internal/boxcli/generate.go
@@ -100,7 +100,7 @@ func direnvCmd() *cobra.Command {
 	command.Flags().BoolVarP(
 		&flags.force, "force", "f", false, "force overwrite existing files")
 	command.Flags().BoolVarP(
-		&flags.envrcContent, "envrc", "e", false, "output contents of a configuration to use in .envrc")
+		&flags.envrcContent, "envrc-fn", "e", false, "output contents of devbox configuration to use in .envrc")
 	flags.config.register(command)
 	return command
 }
