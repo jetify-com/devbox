88feade6e84df98cc2fa3238bb09bd1467a58bff
Zhizhen He <hezhizhen.yi@gmail.com>
2023-05-06 02:03:10 +0800

    chore: use constants defined in envir in version check (#976)
    ## Summary
    
    1. replace constants with existing envir constants
    2. add new envir constants
    
    ## How was it tested?

--- internal/vercheck/vercheck_test.go
+++ internal/vercheck/vercheck_test.go
@@ -1,28 +1,33 @@
+// Copyright 2023 Jetpack Technologies Inc and contributors. All rights reserved.
+// Use of this source code is governed by the license in the LICENSE file.
+
 package vercheck
 
 import (
 	"bytes"
 	"strings"
 	"testing"
+
+	"go.jetpack.io/devbox/internal/envir"
 )
 
 func TestCheckVersion(t *testing.T) {
 
 	t.Run("no_devbox_cloud", func(t *testing.T) {
 		// if devbox cloud
-		t.Setenv("DEVBOX_REGION", "true")
+		t.Setenv(envir.DevboxRegion, "true")
 		buf := new(bytes.Buffer)
 		CheckVersion(buf)
 		if buf.String() != "" {
 			t.Errorf("expected empty string, got %q", buf.String())
 		}
-		t.Setenv("DEVBOX_REGION", "")
+		t.Setenv(envir.DevboxRegion, "")
 	})
 
-	// no envir.LauncherVersion or latest-version env var
+	// no launcher version or latest-version env var
 	t.Run("no_launcher_version_or_latest_version", func(t *testing.T) {
-		t.Setenv("LAUNCHER_VERSION", "")
-		t.Setenv(envDevboxLatestVersion, "")
+		t.Setenv(envir.LauncherVersion, "")
+		t.Setenv(envir.DevboxLatestVersion, "")
 		buf := new(bytes.Buffer)
 		CheckVersion(buf)
 		if buf.String() != "" {
@@ -32,7 +37,7 @@ func TestCheckVersion(t *testing.T) {
 
 	t.Run("launcher_version_outdated", func(t *testing.T) {
 		// set older launcher version
-		t.Setenv("LAUNCHER_VERSION", "v0.1.0")
+		t.Setenv(envir.LauncherVersion, "v0.1.0")
 
 		buf := new(bytes.Buffer)
 		CheckVersion(buf)
@@ -43,10 +48,10 @@ func TestCheckVersion(t *testing.T) {
 
 	t.Run("binary_version_outdated", func(t *testing.T) {
 		// set the launcher version so that it is not outdated
-		t.Setenv("LAUNCHER_VERSION", strings.TrimPrefix(expectedLauncherVersion, "v"))
+		t.Setenv(envir.LauncherVersion, strings.TrimPrefix(expectedLauncherVersion, "v"))
 
 		// set the latest version to be greater the current binary version
-		t.Setenv(envDevboxLatestVersion, "0.4.9")
+		t.Setenv(envir.DevboxLatestVersion, "0.4.9")
 
 		// mock the existing binary version
 		currentDevboxVersion = "v0.4.8"
@@ -61,13 +66,13 @@ func TestCheckVersion(t *testing.T) {
 	t.Run("all_versions_up_to_date", func(t *testing.T) {
 
 		// set the launcher version so that it is not outdated
-		t.Setenv("LAUNCHER_VERSION", strings.TrimPrefix(expectedLauncherVersion, "v"))
+		t.Setenv(envir.LauncherVersion, strings.TrimPrefix(expectedLauncherVersion, "v"))
 
 		// mock the existing binary version
 		currentDevboxVersion = "v0.4.8"
 
 		// set the latest version to the same as the current binary version
-		t.Setenv(envDevboxLatestVersion, "0.4.8")
+		t.Setenv(envir.DevboxLatestVersion, "0.4.8")
 
 		buf := new(bytes.Buffer)
 		CheckVersion(buf)
