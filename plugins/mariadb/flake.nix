ae17639e7e4eb623d349849b129e8e4ae238c390
John Lago <750845+Lagoja@users.noreply.github.com>
2023-05-24 17:28:06 -0700

    add wrappers in flake.nix

---
+++ plugins/mariadb/flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "A flake that outputs MariaDB with custom configuration and aliases to work in Devbox";
+
+  inputs = {
+    nixpkgs.url = "{{.URLForInput}}";
+  };
+
+  outputs = {self, nixpkgs}:
+    let
+      mariadb-bin =  nixpkgs.legacyPackages.{{.System}}.symlinkJoin {
+
+        name = "mariadb-wrapped";
+        paths = [nixpkgs.legacyPackages.{{ .System }}.mariadb];
+        nativeBuildInputs = [ nixpkgs.legacyPackages.{{.System}}.makeWrapper ];
+        postBuild = ''
+          wrapProgram "$out/bin/mysql" \
+            --add-flags '--socket=''$MYSQL_UNIX_PORT';
+
+          wrapProgram $out/bin/mysqladmin \
+            --add-flags "--datadir=''$MYSQL_DATADIR --pid-file=''$MYSQL_PID_FILE --socket=''$MYSQL_UNIX_PORT";
+
+          wrapProgram $out/bin/mysqld \
+            --add-flags "--datadir=''$MYSQL_DATADIR --pid-file=''$MYSQL_PID_FILE --socket=''$MYSQL_UNIX_PORT";
+
+          wrapProgram "$out/bin/mysql_install_db" \
+            --add-flags '--datadir=''$MYSQL_DATADIR --pid-file=''$MYSQL_PID_FILE --basedir=''$MYSQL_BASEDIR';
+        '';
+      };
+    in{
+      packages.{{.System}} = {
+        default = mariadb-bin;
+      };
+    };
+}
