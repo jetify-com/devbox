89804f922b525febf51929f08aa4d690a6a99521
John Lago <750845+Lagoja@users.noreply.github.com>
2023-05-25 18:50:20 -0700

    MariaDB Plugin (#1048)

---
+++ plugins/mariadb/flake.nix
@@ -0,0 +1,45 @@
+{
+  description = "A flake that outputs MariaDB with custom configuration and aliases to work in Devbox";
+
+  inputs = {
+    nixpkgs.url = "{{.URLForInput}}";
+  };
+
+  outputs = {self, nixpkgs}:
+    let
+      mariadb-bin =  nixpkgs.legacyPackages.{{.System}}.symlinkJoin {
+
+        name = "mariadb-wrapped";
+        paths = [nixpkgs.legacyPackages.{{ .System }}.{{.PackageName}}];
+        nativeBuildInputs = [ nixpkgs.legacyPackages.{{.System}}.makeWrapper];
+        postBuild = ''
+
+          wrapProgram $out/bin/mysqld \
+            --add-flags '--datadir=''$MYSQL_DATADIR --pid-file=''$MYSQL_PID_FILE --socket=''$MYSQL_UNIX_PORT';
+
+          wrapProgram $out/bin/mariadbd \
+            --add-flags '--datadir=''$MYSQL_DATADIR --pid-file=''$MYSQL_PID_FILE --socket=''$MYSQL_UNIX_PORT';
+
+          wrapProgram $out/bin/mysqld_safe \
+            --add-flags '--datadir=''$MYSQL_DATADIR --pid-file=''$MYSQL_PID_FILE --socket=''$MYSQL_UNIX_PORT';
+
+          if [-f $out/bin/mariadbd-safe]; then
+            wrapProgram $out/bin/mariadbd_safe \
+              --add-flags '--datadir=''$MYSQL_DATADIR --pid-file=''$MYSQL_PID_FILE --socket=''$MYSQL_UNIX_PORT';
+          fi
+
+          wrapProgram "$out/bin/mysql_install_db" \
+            --add-flags '--datadir=''$MYSQL_DATADIR --pid-file=''$MYSQL_PID_FILE --basedir=''$MYSQL_BASEDIR';
+
+          if [-f $out/bin/mariadb-install-db]; then
+            wrapProgram "$out/bin/mariadb_install_db" \
+              --add-flags '--datadir=''$MYSQL_DATADIR --pid-file=''$MYSQL_PID_FILE --basedir=''$MYSQL_BASEDIR';
+          fi
+        '';
+      };
+    in{
+      packages.{{.System}} = {
+        default = mariadb-bin;
+      };
+    };
+}
