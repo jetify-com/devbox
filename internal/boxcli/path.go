79258568e9cb99668e01995638a8b8647c3482ae
Mike Landau <mikeland86@gmail.com>
2023-06-01 10:58:56 -0700

    [path] Add global path command (#1077)
    ## Summary
    
    Inspired by https://github.com/jetpack-io/devbox/pull/1058
    
    ## How was it tested?
    
    ```bash
    ➜  devbox git:(landau/global-path) devbox global path
    /Users/mike/.local/share/devbox/global/default
    ```

---
+++ internal/boxcli/path.go
@@ -0,0 +1,32 @@
+// Copyright 2023 Jetpack Technologies Inc and contributors. All rights reserved.
+// Use of this source code is governed by the license in the LICENSE file.
+
+package boxcli
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+type pathCmdFlags struct {
+	config configFlags
+}
+
+func pathCmd() *cobra.Command {
+	flags := pathCmdFlags{}
+	cmd := &cobra.Command{
+		Use:     "path",
+		Short:   "Show path to [global] devbox config",
+		PreRunE: ensureNixInstalled,
+		Args:    cobra.ExactArgs(0),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			fmt.Println(flags.config.path)
+			return nil
+		},
+	}
+
+	flags.config.register(cmd)
+
+	return cmd
+}
