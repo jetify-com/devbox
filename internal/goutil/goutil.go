8fceab7ced68fc7cde2537ee8d6fe25aade323e4
Mike Landau <mikeland86@gmail.com>
2023-06-06 12:25:46 -0700

    Revert goutil changes

--- internal/goutil/goutil.go
+++ internal/goutil/goutil.go
@@ -3,22 +3,10 @@
 
 package goutil
 
-import "github.com/samber/lo"
-
 func PickByKeysSorted[K comparable, V any](in map[K]V, keys []K) []V {
 	out := make([]V, len(keys))
 	for i, key := range keys {
 		out[i] = in[key]
-	}
-	return out
-}
-
-func Except[T comparable](in []T, except ...T) []T {
-	out := []T{}
-	for _, item := range in {
-		if !lo.Contains(except, item) {
-			out = append(out, item)
-		}
 	}
 	return out
 }
