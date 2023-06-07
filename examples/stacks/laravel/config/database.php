9385ad018a70d9a5c0491ab444df509c1cee369d
John Lago <750845+Lagoja@users.noreply.github.com>
2023-06-06 21:11:00 -0700

    re-org, add mysql + redis

--- examples/stacks/laravel/config/database.php
+++ examples/stacks/laravel/config/database.php
@@ -45,13 +45,12 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
+            'unix_socket' => env('MYSQL_UNIX_PORT', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
