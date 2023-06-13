479ab217ee79b1fcc5e4f0b49c29eae8f9a112c9
mohsenari <mohsen@mailbox.org>
2023-06-13 12:02:55 -0400

    more cleanup

--- vscode-extension/src/devbox.ts
+++ vscode-extension/src/devbox.ts
@@ -6,57 +6,57 @@ interface Message {
     status: string
 }
 export async function devboxReopen() {
-    if (workspace.workspaceFolders) {
-        await window.withProgress({
-            location: ProgressLocation.Notification,
-            title: "Setting up your Devbox environment. Please don't close vscode.",
-            cancellable: true
-        },
-            async (progress, token) => {
-                token.onCancellationRequested(() => {
-                    console.log("User canceled the long running operation");
-                });
+    await window.withProgress({
+        location: ProgressLocation.Notification,
+        title: "Setting up your Devbox environment. Please don't close vscode.",
+        cancellable: true
+    },
+        async (progress, token) => {
+            token.onCancellationRequested(() => {
+                console.log("User canceled the long running operation");
+            });
 
-                const p = new Promise<void>(async (resolve, reject) => {
+            const p = new Promise<void>(async (resolve, reject) => {
 
-                    if (workspace.workspaceFolders) {
-                        const workingDir = workspace.workspaceFolders[0].uri;
-                        const dotdevbox = Uri.joinPath(workingDir, '/.devbox');
-                        try {
-                            // check if .devbox exists
-                            await workspace.fs.stat(dotdevbox);
-                        } catch (error) {
-                            //.devbox doesn't exist
-                            // running devbox shellenv to create it
-                            spawnSync('devbox', ['shellenv'], {
-                                cwd: workingDir.path
-                            });
-                        }
-                        // run devbox integrate and then close this window
-                        const devbox = '/Users/mohsenansari/code/jetpack/go.jetpack.io/examples/vscode/vscodetest/devbox';
-                        // const devbox = 'devbox'
-                        let child = spawn(devbox, ['integrate', 'vscode'], {
-                            cwd: workingDir.path,
-                            stdio: [0, 1, 2, 'ipc']
+                if (workspace.workspaceFolders) {
+                    const workingDir = workspace.workspaceFolders[0].uri;
+                    const dotdevbox = Uri.joinPath(workingDir, '/.devbox');
+                    try {
+                        // check if .devbox exists
+                        await workspace.fs.stat(dotdevbox);
+                    } catch (error) {
+                        //.devbox doesn't exist
+                        // running devbox shellenv to create it
+                        spawnSync('devbox', ['shellenv'], {
+                            cwd: workingDir.path
                         });
-                        child.on('close', (code: number) => {
-                            if (code === 1) {
-                                window.showErrorMessage("Failed to setup devbox environment.");
-                                reject();
-                            }
-                        });
-                        child.send({ configDir: workingDir.path });
-                        child.on('message', function (msg: Message, handle) {
-                            if (msg.status === "finished") {
-                                resolve();
-                                commands.executeCommand("workbench.action.closeWindow");
-                            }
-                        });
-
                     }
-                });
-                return p;
-            }
-        );
-    }
+                    // run devbox integrate and then close this window
+                    const devbox = '/Users/mohsenansari/code/jetpack/go.jetpack.io/examples/vscode/vscodetest/devbox';
+                    // const devbox = 'devbox'
+                    let child = spawn(devbox, ['integrate', 'vscode'], {
+                        cwd: workingDir.path,
+                        stdio: [0, 1, 2, 'ipc']
+                    });
+                    // if CLI closes before sending "finished" message
+                    child.on('close', (code: number) => {
+                        if (code === 1) {
+                            window.showErrorMessage("Failed to setup devbox environment.");
+                            reject();
+                        }
+                    });
+                    // send config path to CLI
+                    child.send({ configDir: workingDir.path });
+                    // handle CLI finishing the env and sending  "finished"
+                    child.on('message', function (msg: Message, handle) {
+                        if (msg.status === "finished") {
+                            resolve();
+                            commands.executeCommand("workbench.action.closeWindow");
+                        }
+                    });
+                }
+            });
+            return p;
+        }
+    );
 }
