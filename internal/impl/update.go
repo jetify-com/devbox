e61962c018c825a5027e200b8888cfbf361e5b99
Mike Landau <mikeland86@gmail.com>
2023-06-19 15:26:45 -0400

    Simplify code

--- internal/impl/update.go
+++ internal/impl/update.go
@@ -45,27 +45,11 @@ func (d *Devbox) Update(ctx context.Context, pkgs ...string) error {
 			if err = d.attemptToUpgradeFlake(pkg); err != nil {
 				return err
 			}
-			continue
-		}
-		existing := d.lockfile.Packages[pkg.Raw]
-		newEntry, err := searcher.Client().Resolve(pkg.Raw)
-		if err != nil {
-			return err
-		}
-		if existing != nil && existing.Version != newEntry.Version {
-			ux.Finfo(d.writer, "Updating %s %s -> %s\n", pkg, existing.Version, newEntry.Version)
-			if err := d.removePackagesFromProfile(ctx, []string{pkg.Raw}); err != nil {
-				// Warn but continue. TODO(landau): ensurePackagesAreInstalled should
-				// sync the profile so we don't need to do this manually.
-				ux.Fwarning(d.writer, "Failed to remove %s from profile: %s\n", pkg, err)
-			}
-		} else if existing == nil {
-			ux.Finfo(d.writer, "Resolved %s to %[1]s %[2]s\n", pkg, newEntry.Resolved)
 		} else {
-			ux.Finfo(d.writer, "Already up-to-date %s %s\n", pkg, existing.Version)
+			if err = d.updateDevboxPackage(ctx, pkg); err != nil {
+				return err
+			}
 		}
-		// Set the new entry after we've removed the old package from the profile
-		d.lockfile.Packages[pkg.Raw] = newEntry
 	}
 
 	// TODO(landau): Improve output
@@ -90,6 +74,32 @@ func (d *Devbox) inputsToUpdate(pkgs ...string) ([]*nix.Input, error) {
 	}
 
 	return nix.InputsFromStrings(pkgsToUpdate, d.lockfile), nil
+}
+
+func (d *Devbox) updateDevboxPackage(
+	ctx context.Context,
+	pkg *nix.Input,
+) error {
+	existing := d.lockfile.Packages[pkg.Raw]
+	newEntry, err := searcher.Client().Resolve(pkg.Raw)
+	if err != nil {
+		return err
+	}
+	if existing != nil && existing.Version != newEntry.Version {
+		ux.Finfo(d.writer, "Updating %s %s -> %s\n", pkg, existing.Version, newEntry.Version)
+		if err := d.removePackagesFromProfile(ctx, []string{pkg.Raw}); err != nil {
+			// Warn but continue. TODO(landau): ensurePackagesAreInstalled should
+			// sync the profile so we don't need to do this manually.
+			ux.Fwarning(d.writer, "Failed to remove %s from profile: %s\n", pkg, err)
+		}
+	} else if existing == nil {
+		ux.Finfo(d.writer, "Resolved %s to %[1]s %[2]s\n", pkg, newEntry.Resolved)
+	} else {
+		ux.Finfo(d.writer, "Already up-to-date %s %s\n", pkg, existing.Version)
+	}
+	// Set the new entry after we've removed the old package from the profile
+	d.lockfile.Packages[pkg.Raw] = newEntry
+	return nil
 }
 
 // attemptToUpgradeFlake attempts to upgrade a flake using `nix profile upgrade`
