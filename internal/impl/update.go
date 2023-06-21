d43b2c900d5a6e91d89163c904eac789bc8eb1f7
Mike Landau <mikeland86@gmail.com>
2023-06-21 09:04:27 -0400

    [update] Update flakes (#1180)
    ## Summary
    
    Update flakes via `nix profile upgrade`.
    
    Purposefully forgiving.
    
    Note: local flakes don't need updating because they automatically use
    latest version of file.
    
    fixes https://github.com/jetpack-io/devbox/issues/1094
    
    ## How was it tested?
    
    ```bash
    devbox add github:F1bonacc1/process-compose 
    devbox update github:F1bonacc1/process-compose
    ```
    
    This works, but but need to test something that will actually update.

--- internal/impl/update.go
+++ internal/impl/update.go
@@ -42,28 +42,14 @@ func (d *Devbox) Update(ctx context.Context, pkgs ...string) error {
 
 	for _, pkg := range pendingPackagesToUpdate {
 		if _, _, isVersioned := devpkg.ParseVersionedPackage(pkg.Raw); !isVersioned {
-			fmt.Fprintf(d.writer, "Skipping %s because it is not a versioned package\n", pkg)
-			continue
-		}
-		existing := d.lockfile.Packages[pkg.Raw]
-		newEntry, err := searcher.Client().Resolve(pkg.Raw)
-		if err != nil {
-			return err
-		}
-		if existing != nil && existing.Version != newEntry.Version {
-			fmt.Fprintf(d.writer, "Updating %s %s -> %s\n", pkg, existing.Version, newEntry.Version)
-			if err := d.removePackagesFromProfile(ctx, []string{pkg.Raw}); err != nil {
-				// Warn but continue. TODO(landau): ensurePackagesAreInstalled should
-				// sync the profile so we don't need to do this manually.
-				ux.Fwarning(d.writer, "Failed to remove %s from profile: %s\n", pkg, err)
+			if err = d.attemptToUpgradeFlake(pkg); err != nil {
+				return err
 			}
-		} else if existing == nil {
-			fmt.Fprintf(d.writer, "Resolved %s to %[1]s %[2]s\n", pkg, newEntry.Resolved)
 		} else {
-			fmt.Fprintf(d.writer, "Already up-to-date %s %s\n", pkg, existing.Version)
+			if err = d.updateDevboxPackage(ctx, pkg); err != nil {
+				return err
+			}
 		}
-		// Set the new entry after we've removed the old package from the profile
-		d.lockfile.Packages[pkg.Raw] = newEntry
 	}
 
 	// TODO(landau): Improve output
@@ -88,4 +74,57 @@ func (d *Devbox) inputsToUpdate(pkgs ...string) ([]*nix.Input, error) {
 	}
 
 	return nix.InputsFromStrings(pkgsToUpdate, d.lockfile), nil
+}
+
+func (d *Devbox) updateDevboxPackage(
+	ctx context.Context,
+	pkg *nix.Input,
+) error {
+	existing := d.lockfile.Packages[pkg.Raw]
+	newEntry, err := searcher.Client().Resolve(pkg.Raw)
+	if err != nil {
+		return err
+	}
+	if existing != nil && existing.Version != newEntry.Version {
+		ux.Finfo(d.writer, "Updating %s %s -> %s\n", pkg, existing.Version, newEntry.Version)
+		if err := d.removePackagesFromProfile(ctx, []string{pkg.Raw}); err != nil {
+			// Warn but continue. TODO(landau): ensurePackagesAreInstalled should
+			// sync the profile so we don't need to do this manually.
+			ux.Fwarning(d.writer, "Failed to remove %s from profile: %s\n", pkg, err)
+		}
+	} else if existing == nil {
+		ux.Finfo(d.writer, "Resolved %s to %[1]s %[2]s\n", pkg, newEntry.Resolved)
+	} else {
+		ux.Finfo(d.writer, "Already up-to-date %s %s\n", pkg, existing.Version)
+	}
+	// Set the new entry after we've removed the old package from the profile
+	d.lockfile.Packages[pkg.Raw] = newEntry
+	return nil
+}
+
+// attemptToUpgradeFlake attempts to upgrade a flake using `nix profile upgrade`
+// and prints an error if it fails, but does not propagate upgrade errors.
+func (d *Devbox) attemptToUpgradeFlake(pkg *nix.Input) error {
+	profilePath, err := d.profilePath()
+	if err != nil {
+		return err
+	}
+
+	ux.Finfo(
+		d.writer,
+		"Attempting to upgrade %s using `nix profile upgrade`\n",
+		pkg.Raw,
+	)
+
+	err = nix.Upgrade(profilePath, pkg, d.lockfile)
+	if err != nil {
+		ux.Ferror(
+			d.writer,
+			"Failed to upgrade %s using `nix profile upgrade`: %s\n",
+			pkg.Raw,
+			err,
+		)
+	}
+
+	return nil
 }
