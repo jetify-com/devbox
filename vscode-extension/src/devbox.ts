7fb8c2bc08f16d26b2fc902a65eeb239b345cbd2
mohsenari <mohsenari@users.noreply.github.com>
2023-06-23 16:00:44 -0400

    [vscode-extension] fix vscode terminal PATH  (#1199)
    ## Summary
    Fixes the issue where terminal's $PATH env variable wouldn't get updated
    in integrated terminal.
    
    ## How was it tested?
    buckle up, the steps are long:
    
    - `cd vscode-extension`
    - `vsce package` (install vsce via `npm i -g vsce`)
    - `code --install-extension devbox-0.0.7.vsix`
    - setup a devbox.json in a sample directory with a couple of packages
    (e.g., `hello`, `go@1.18`)
    - open the sample directory in vscode
    - cmd+shift+p and run `Devbox: Reopen in Devbox shell environment`
    - after vscode reopens, echo $PATH in terminal

--- vscode-extension/src/devbox.ts
+++ vscode-extension/src/devbox.ts
@@ -1,10 +1,11 @@
-import { window, workspace, commands, ProgressLocation, Uri } from 'vscode';
+import { window, workspace, commands, ProgressLocation, Uri, ConfigurationTarget } from 'vscode';
 import { spawn, spawnSync } from 'node:child_process';
 
 
 interface Message {
     status: string
 }
+
 export async function devboxReopen() {
     await window.withProgress({
         location: ProgressLocation.Notification,
@@ -21,16 +22,15 @@ export async function devboxReopen() {
                 if (workspace.workspaceFolders) {
                     const workingDir = workspace.workspaceFolders[0].uri;
                     const dotdevbox = Uri.joinPath(workingDir, '/.devbox');
-                    try {
-                        // check if .devbox exists
-                        await workspace.fs.stat(dotdevbox);
-                    } catch (error) {
-                        //.devbox doesn't exist
-                        // running devbox shellenv to create it
-                        spawnSync('devbox', ['shellenv'], {
-                            cwd: workingDir.path
-                        });
-                    }
+                    progress.report({ message: 'Installing devbox packages...', increment: 25 });
+                    await setupDotDevbox(workingDir, dotdevbox);
+
+                    // setup required vscode settings
+                    progress.report({ message: 'Updating configurations...', increment: 50 });
+                    updateVSCodeConf();
+
+                    // Calling CLI to compute devbox env
+                    progress.report({ message: 'Calling Devbox to setup environment...', increment: 80 });
                     // To use a custom compiled devbox when testing, change this to an absolute path.
                     const devbox = 'devbox';
                     // run devbox integrate and then close this window
@@ -49,8 +49,14 @@ export async function devboxReopen() {
                     // handle CLI finishing the env and sending  "finished"
                     child.on('message', function (msg: Message, handle) {
                         if (msg.status === "finished") {
+                            progress.report({ message: 'Finished setting up! Reloading the window...', increment: 100 });
                             resolve();
                             commands.executeCommand("workbench.action.closeWindow");
+                        }
+                        else {
+                            console.log(msg);
+                            window.showErrorMessage("Failed to setup devbox environment.");
+                            reject();
                         }
                     });
                 }
@@ -58,4 +64,56 @@ export async function devboxReopen() {
             return p;
         }
     );
+}
+
+async function setupDotDevbox(workingDir: Uri, dotdevbox: Uri) {
+    try {
+        // check if .devbox exists
+        await workspace.fs.stat(dotdevbox);
+    } catch (error) {
+        //.devbox doesn't exist
+        // running devbox shellenv to create it
+        spawnSync('devbox', ['shellenv'], {
+            cwd: workingDir.path
+        });
+    }
+}
+
+function updateVSCodeConf() {
+    if (process.platform === 'darwin') {
+        const shell = process.env["SHELL"] ?? "/bin/zsh";
+        const shellArgsMap = (shellType: string) => {
+            switch (shellType) {
+                case "fish":
+                    // We special case fish here because fish's `fish_add_path` function
+                    // tends to prepend to PATH by default, hence sourcing the fish config after
+                    // vscode reopens in devbox environment, overwrites devbox packages and 
+                    // might cause confusion for users as to why their system installed packages
+                    // show up when they type for example `which go` as opposed to the packages
+                    // installed by devbox.
+                    return ["--no-config"];
+                default:
+                    return [];
+            }
+        };
+        const shellTypeSlices = shell.split("/");
+        const shellType = shellTypeSlices[shellTypeSlices.length - 1];
+        shellArgsMap(shellType);
+        const devboxCompatibleShell = {
+            "devboxCompatibleShell": {
+                "path": shell,
+                "args": shellArgsMap(shellType)
+            }
+        };
+
+        workspace.getConfiguration().update(
+            'terminal.integrated.profiles.osx',
+            devboxCompatibleShell,
+            ConfigurationTarget.Workspace
+        );
+        workspace.getConfiguration().update(
+            'terminal.integrated.defaultProfile.osx',
+            'devboxCompatibleShell',
+            ConfigurationTarget.Workspace);
+    }
 }
