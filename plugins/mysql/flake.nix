9abfb6d54f0fbac9d9b103d74ac15d1ff7347081
John Lago <750845+Lagoja@users.noreply.github.com>
2023-06-26 15:00:41 -0700

    initial plugin + example

---
+++ plugins/mysql/flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "A flake that outputs MySQL with custom configuration and aliases to work in Devbox";
+
+  inputs = {
+    nixpkgs.url = "{{.URLForInput}}";
+  };
+
+  outputs = {self, nixpkgs}:
+    let
+      mysql-bin =  nixpkgs.legacyPackages.{{.System}}.symlinkJoin {
+
+        name = "mysql-wrapped";
+        paths = [nixpkgs.legacyPackages.{{ .System }}.{{.PackageAttributePath}}];
+        nativeBuildInputs = [ nixpkgs.legacyPackages.{{.System}}.makeWrapper];
+        postBuild = ''
+
+          wrapProgram $out/bin/mysqld \
+            --add-flags '--datadir=''$MYSQL_DATADIR --pid-file=''$MYSQL_PID_FILE --socket=''$MYSQL_UNIX_PORT';
+
+          wrapProgram $out/bin/mysqld_safe \
+            --add-flags '--datadir=''$MYSQL_DATADIR --pid-file=''$MYSQL_PID_FILE --socket=''$MYSQL_UNIX_PORT';
+        '';
+      };
+    in{
+      packages.{{.System}} = {
+        default = mysql-bin;
+      };
+    };
+}
