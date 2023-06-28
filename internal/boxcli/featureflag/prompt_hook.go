09250e680033cc3093c0eccd16dccd4b170ecdd4
Savil Srivastava <676452+savil@users.noreply.github.com>
2023-06-28 13:20:39 -0700

    update newer feature flags

--- internal/boxcli/featureflag/prompt_hook.go
+++ internal/boxcli/featureflag/prompt_hook.go
@@ -2,4 +2,4 @@ package featureflag
 
 // PromptHook controls the insertion of a shell prompt hook that invokes
 // devbox shellenv, in lieu of using binary wrappers.
-var PromptHook = disabled("PROMPT_HOOK")
+var PromptHook = disable("PROMPT_HOOK")
