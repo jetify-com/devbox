08029df25927a9f6fd7132fb57aef142015f66c6
Mike Landau <mikeland86@gmail.com>
2023-07-03 15:17:55 -0700

    Fix search command

--- internal/boxcli/search.go
+++ internal/boxcli/search.go
@@ -11,8 +11,10 @@ import (
 
 	"github.com/spf13/cobra"
 
+	"go.jetpack.io/devbox/internal/boxcli/usererr"
 	"go.jetpack.io/devbox/internal/searcher"
 	"go.jetpack.io/devbox/internal/searcher/model"
+	"go.jetpack.io/devbox/internal/ux"
 )
 
 type searchCmdFlags struct {
@@ -27,11 +29,29 @@ func searchCmd() *cobra.Command {
 		Args:  cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			query := args[0]
-			results, err := searcher.Client().Search(query)
-			if err != nil {
-				return err
+			name, version, isVersioned := searcher.ParseVersionedPackage(query)
+			if !isVersioned {
+				results, err := searcher.Client().Search(query)
+				if err != nil {
+					return err
+				}
+				return printSearchResults(
+					cmd.OutOrStdout(), query, results, flags.showAll)
 			}
-			return printSearchResults(cmd.OutOrStdout(), query, results, flags.showAll)
+			packageVersion, err := searcher.Client().Resolve(name, version)
+			if err != nil {
+				// This is not ideal. Search service should return valid response we
+				// can parse
+				return usererr.WithUserMessage(err, "No results found for %q\n", query)
+			}
+			fmt.Fprintf(
+				cmd.OutOrStdout(),
+				"%s results to: %s@%s\n",
+				query,
+				packageVersion.Name,
+				packageVersion.Version,
+			)
+			return nil
 		},
 	}
 
@@ -60,18 +80,20 @@ func printSearchResults(
 		query,
 	)
 
+	resultsAreTrimmed := false
 	pkgs := results.Packages
 	if !showAll && len(pkgs) > 10 {
-		fmt.Fprint(
-			w,
-			"Showing top 10 results. Use --show-all to show all.\n\n",
-		)
+		resultsAreTrimmed = true
 		pkgs = results.Packages[:int(math.Min(10, float64(len(results.Packages))))]
 	}
 
-	for _, r := range pkgs {
+	for _, pkg := range pkgs {
 		nonEmptyVersions := []string{}
-		for _, v := range r.Versions {
+		for i, v := range pkg.Versions {
+			if !showAll && i >= 10 {
+				resultsAreTrimmed = true
+				break
+			}
 			if v.Version != "" {
 				nonEmptyVersions = append(nonEmptyVersions, v.Version)
 			}
@@ -81,7 +103,17 @@ func printSearchResults(
 		if len(nonEmptyVersions) > 0 {
 			versionString = fmt.Sprintf(" (%s)", strings.Join(nonEmptyVersions, ", "))
 		}
-		fmt.Fprintf(w, "* %s %s\n", r.Name, versionString)
+		fmt.Fprintf(w, "* %s %s\n", pkg.Name, versionString)
 	}
+
+	if resultsAreTrimmed {
+		fmt.Println()
+		ux.Fwarning(
+			w,
+			"Showing top 10 results and truncated versions. Use --show-all to "+
+				"show all.\n\n",
+		)
+	}
+
 	return nil
 }
