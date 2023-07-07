bfd8e5905c0394587133f6aa82393d114fed0f16
Savil Srivastava <676452+savil@users.noreply.github.com>
2023-07-06 22:28:41 -0700

    use --json for nix store make-content-addressed

--- internal/nix/store.go
+++ internal/nix/store.go
@@ -1,8 +1,8 @@
 package nix
 
 import (
+	"encoding/json"
 	"path/filepath"
-	"regexp"
 	"strings"
 
 	"github.com/pkg/errors"
@@ -17,26 +17,28 @@ func StorePath(hash, name, version string) string {
 	return filepath.Join("/nix/store", storeDir)
 }
 
-// contentAddressedRegex matches the output of `nix store make-content-addressed`.
-// It is used to select the content-addressed store path (the second one in the example below).
-//
-// Example:
-// > nix store make-content-addressed  /nix/store/r2jd6ygnmirm2g803mksqqjm4y39yi6i-git-2.33.1
-// rewrote '/nix/store/r2jd6ygnmirm2g803mksqqjm4y39yi6i-git-2.33.1' to '/nix/store/ldbhlwhh39wha58rm61bkiiwm6j7211j-git-2.33.1'
-var contentAddressedRegex = regexp.MustCompile(`rewrote\s'[\/a-z0-9-\.]+'\sto\s'([a-z0-9-\/\.]+)'`)
-
 // ContentAddressedStorePath takes a store path and returns the content-addressed store path.
 func ContentAddressedStorePath(storePath string) (string, error) {
-	cmd := command("store", "make-content-addressed", storePath)
-	out, err := cmd.CombinedOutput()
+	cmd := command("store", "make-content-addressed", storePath, "--json")
+	out, err := cmd.Output()
 	if err != nil {
 		return "", errors.WithStack(err)
 	}
+	// Example Output:
+	// > nix store make-content-addressed /nix/store/r2jd6ygnmirm2g803mksqqjm4y39yi6i-git-2.33.1 --json
+	// {"rewrites":{"/nix/store/r2jd6ygnmirm2g803mksqqjm4y39yi6i-git-2.33.1":"/nix/store/ldbhlwhh39wha58rm61bkiiwm6j7211j-git-2.33.1"}}
 
-	matches := contentAddressedRegex.FindStringSubmatch(string(out))
-	if len(matches) < 2 {
-		return "", errors.Errorf("could not parse output of nix store make-content-addressed: %s", string(out))
+	type ContentAddressed struct {
+		Rewrites map[string]string `json:"rewrites"`
+	}
+	caOutput := ContentAddressed{}
+	if err := json.Unmarshal(out, &caOutput); err != nil {
+		return "", errors.WithStack(err)
 	}
 
-	return matches[1], nil
+	caStorePath, ok := caOutput.Rewrites[storePath]
+	if !ok {
+		return "", errors.Errorf("could not find content-addressed store path for %s", storePath)
+	}
+	return caStorePath, nil
 }
