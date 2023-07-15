33135edda8f6c79418fe97366672a17eae4f5741
mohsenari <mohsen@mailbox.org>
2023-07-14 22:14:29 -0400

    Small fix

--- vscode-extension/src/devbox.ts
+++ vscode-extension/src/devbox.ts
@@ -17,65 +17,67 @@ export async function devboxReopen() {
         if (result === seeDocs) {
             env.openExternal(Uri.parse('https://www.jetpack.io/devbox/docs/ide_configuration/vscode/#windows-setup'));
         }
-    }
+    } else {
+        await window.withProgress({
+            location: ProgressLocation.Notification,
+            title: "Setting up your Devbox environment. Please don't close vscode.",
+            cancellable: true
+        },
+            async (progress, token) => {
+                token.onCancellationRequested(() => {
+                    console.log("User canceled the long running operation");
+                });
 
-    await window.withProgress({
-        location: ProgressLocation.Notification,
-        title: "Setting up your Devbox environment. Please don't close vscode.",
-        cancellable: true
-    },
-        async (progress, token) => {
-            token.onCancellationRequested(() => {
-                console.log("User canceled the long running operation");
-            });
+                const p = new Promise<void>(async (resolve, reject) => {
 
-            const p = new Promise<void>(async (resolve, reject) => {
+                    if (workspace.workspaceFolders) {
+                        const workingDir = workspace.workspaceFolders[0].uri;
+                        const dotdevbox = Uri.joinPath(workingDir, '/.devbox');
+                        progress.report({ message: 'Installing devbox packages...', increment: 25 });
+                        await setupDotDevbox(workingDir, dotdevbox);
 
-                if (workspace.workspaceFolders) {
-                    const workingDir = workspace.workspaceFolders[0].uri;
-                    const dotdevbox = Uri.joinPath(workingDir, '/.devbox');
-                    progress.report({ message: 'Installing devbox packages...', increment: 25 });
-                    await setupDotDevbox(workingDir, dotdevbox);
+                        // setup required vscode settings
+                        progress.report({ message: 'Updating configurations...', increment: 50 });
+                        updateVSCodeConf();
 
-                    // setup required vscode settings
-                    progress.report({ message: 'Updating configurations...', increment: 50 });
-                    updateVSCodeConf();
-
-                    // Calling CLI to compute devbox env
-                    progress.report({ message: 'Calling Devbox to setup environment...', increment: 80 });
-                    // To use a custom compiled devbox when testing, change this to an absolute path.
-                    const devbox = 'devbox';
-                    // run devbox integrate and then close this window
-                    let child = spawn(devbox, ['integrate', 'vscode'], {
-                        cwd: workingDir.path,
-                        stdio: [0, 1, 2, 'ipc']
-                    });
-                    // if CLI closes before sending "finished" message
-                    child.on('close', (code: number) => {
-                        console.log("child process closed with exit code:", code);
-                        window.showErrorMessage("Failed to setup devbox environment.");
-                        reject();
-                    });
-                    // send config path to CLI
-                    child.send({ configDir: workingDir.path });
-                    // handle CLI finishing the env and sending  "finished"
-                    child.on('message', function (msg: Message, handle) {
-                        if (msg.status === "finished") {
-                            progress.report({ message: 'Finished setting up! Reloading the window...', increment: 100 });
-                            resolve();
-                            commands.executeCommand("workbench.action.closeWindow");
-                        }
-                        else {
-                            console.log(msg);
+                        // Calling CLI to compute devbox env
+                        progress.report({ message: 'Calling Devbox to setup environment...', increment: 80 });
+                        // To use a custom compiled devbox when testing, change this to an absolute path.
+                        const devbox = 'devbox';
+                        // run devbox integrate and then close this window
+                        let child = spawn(devbox, ['integrate', 'vscode'], {
+                            cwd: workingDir.path,
+                            stdio: [0, 1, 2, 'ipc']
+                        });
+                        // if CLI closes before sending "finished" message
+                        child.on('close', (code: number) => {
+                            console.log("child process closed with exit code:", code);
                             window.showErrorMessage("Failed to setup devbox environment.");
                             reject();
-                        }
-                    });
-                }
-            });
-            return p;
-        }
-    );
+                        });
+                        // send config path to CLI
+                        child.send({ configDir: workingDir.path });
+                        // handle CLI finishing the env and sending  "finished"
+                        child.on('message', function (msg: Message, handle) {
+                            if (msg.status === "finished") {
+                                progress.report({ message: 'Finished setting up! Reloading the window...', increment: 100 });
+                                resolve();
+                                commands.executeCommand("workbench.action.closeWindow");
+                            }
+                            else {
+                                console.log(msg);
+                                window.showErrorMessage("Failed to setup devbox environment.");
+                                reject();
+                            }
+                        });
+                    }
+                });
+                return p;
+            }
+        );
+    }
+
+
 }
 
 async function setupDotDevbox(workingDir: Uri, dotdevbox: Uri) {
