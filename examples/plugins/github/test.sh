454acfb9f0f585ebc101427c7868014b1f56d1fa
Mike Landau <mikeland86@gmail.com>
2023-07-21 10:04:32 -0700

    [plugins] Fix test

--- examples/plugins/github/test.sh
+++ examples/plugins/github/test.sh
@@ -2,11 +2,10 @@
 
 expected="I AM SET (new value)"
 custom_expected="I AM SET TO CUSTOM (new value)"
-if [ "$MY_ENV_VAR" == "$expected" ] && [ "$MY_ENV_VAR_CUSTOM" == "$MY_ENV_VAR_CUSTOM" ]; then
+if [ "$MY_ENV_VAR" == "$expected" ] && [ "$MY_ENV_VAR_CUSTOM" == "$custom_expected" ]; then
   echo "Success! MY_ENV_VAR is set to '$MY_ENV_VAR'"
   echo "Success! MY_ENV_VAR_CUSTOM is set to '$MY_ENV_VAR_CUSTOM'"
 else
-  echo "MY_ENV_VAR environment variable is not set to '$expected'"
-  echo "MY_ENV_VAR MY_ENV_VAR_CUSTOM variable is not set to '$MY_ENV_VAR_CUSTOM'"
+  echo "ERROR: MY_ENV_VAR environment variable is not set to '$expected' OR MY_ENV_VAR_CUSTOM variable is not set to '$custom_expected'"
   exit 1
 fi
