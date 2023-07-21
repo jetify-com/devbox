ea9a4ad7d64f7a1722d729d7ab28abdb4e5601ca
Mike Landau <mikeland86@gmail.com>
2023-07-21 14:56:54 -0700

    Fix map

--- plugins/builtins.go
+++ plugins/builtins.go
@@ -26,12 +26,12 @@ type BuiltIn struct {
 }
 
 var builtInMap = map[*regexp.Regexp]string{
-	regexp.MustCompile(`^(apache|apacheHttpd)$`):                       "apache",
-	regexp.MustCompile(`^php[0-9]*$`):                                  "php",
+	regexp.MustCompile(`^(apache|apacheHttpd)$`):                       "apacheHttpd",
 	regexp.MustCompile(`^(gradle|gradle_[0-9])$`):                      "gradle",
 	regexp.MustCompile(`^(ghc|haskell\.compiler\.(.*))$`):              "haskell",
 	regexp.MustCompile(`^mariadb(-embedded)?_?[0-9]*$`):                "mariadb",
 	regexp.MustCompile(`^mysql?[0-9]*$`):                               "mysql",
+	regexp.MustCompile(`^php[0-9]*$`):                                  "php",
 	regexp.MustCompile(`^python3[0-9]*Packages.pip$`):                  "pip",
 	regexp.MustCompile(`^postgresql(_[0-9]+)?$`):                       "postgresql",
 	regexp.MustCompile(`^python[0-9]*(Full|Minimal|-full|-minimal)?$`): "python",
