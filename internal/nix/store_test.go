714dbac09df3730f0d8182aeb50f712547bfea17
Greg Curtis <greg.curtis@jetpack.io>
2023-07-26 17:37:29 -0400

    ci: upgrade nix install action, use it in nix-test-versions (#1316)
    Changes extracted from #1311 so they can be merged sooner.
    
    - Upgrade the Nix installer action to v4 and configure the experimental
    features Devbox needs.
    - Print out the Nix version, `/etc/nix/nix.conf`, and the resolved Nix
    config to make debugging easier.
    - Do the same for `test-nix-versions` and add the last 3 Nix releases to
    the matrix. Nix 2.15.1 is the current stable version and 2.17.0 is the
    latest.
    - Update `TestContentAddressedPath` to accept hashes from Nix <2.17 and
    2.17+.

--- internal/nix/store_test.go
+++ internal/nix/store_test.go
@@ -2,18 +2,26 @@ package nix
 
 import (
 	"fmt"
+	"strings"
 	"testing"
+
+	"golang.org/x/exp/slices"
 )
 
 func TestContentAddressedPath(t *testing.T) {
-
 	testCases := []struct {
 		storePath string
-		expected  string
+		expected  []string
 	}{
 		{
 			"/nix/store/r2jd6ygnmirm2g803mksqqjm4y39yi6i-git-2.33.1",
-			"/nix/store/ldbhlwhh39wha58rm61bkiiwm6j7211j-git-2.33.1",
+			[]string{
+				// Hash from before Nix 2.17.0.
+				"/nix/store/ldbhlwhh39wha58rm61bkiiwm6j7211j-git-2.33.1",
+
+				// Hash after Nix 2.17.0.
+				"/nix/store/d49wyvsz5nkqa23qp4p0ikr04mw9n4h9-git-2.33.1",
+			},
 		},
 	}
 
@@ -23,10 +31,9 @@ func TestContentAddressedPath(t *testing.T) {
 			if err != nil {
 				t.Errorf("got error: %v", err)
 			}
-			if out != testCase.expected {
-				t.Errorf("got %s, want %s", out, testCase.expected)
+			if !slices.Contains(testCase.expected, out) {
+				t.Errorf("got %q, want any of:\n%s", out, strings.Join(testCase.expected, "\n"))
 			}
 		})
-
 	}
 }
