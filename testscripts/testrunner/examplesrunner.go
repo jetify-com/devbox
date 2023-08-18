805582ff27467ce343794ef622d81b8efd895003
Savil Srivastava <676452+savil@users.noreply.github.com>
2023-08-18 13:10:04 -0700

    enable servers

--- testscripts/testrunner/examplesrunner.go
+++ testscripts/testrunner/examplesrunner.go
@@ -81,10 +81,10 @@ func RunDevboxTestscripts(t *testing.T, dir string) {
 			"databases",
 			"development",
 			"flakes",
-			//"insecure",
-			//"plugins",
+			"insecure",
+			"plugins",
 			//"servers",
-			//"stacks",
+			"stacks",
 		}
 		for _, ignoredDir := range ignoredExampleDirs {
 			if strings.Contains(path, ignoredDir) {
