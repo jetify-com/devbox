4f7210985a1497263190d2d5e5f26973b3c58224
Savil Srivastava <676452+savil@users.noreply.github.com>
2023-08-18 15:37:01 -0700

    enable rails

--- testscripts/testrunner/examplesrunner.go
+++ testscripts/testrunner/examplesrunner.go
@@ -88,10 +88,10 @@ func RunDevboxTestscripts(t *testing.T, dir string) {
 			"stacks/drupal",
 			"stacks/jekyll",
 			"stacks/lapp-stack",
-			//"stacks/laravel",
-			//"stacks/lepp-stack",
+			"stacks/laravel",
+			"stacks/lepp-stack",
 			//"stacks/rails",
-			//"stacks/spring",
+			"stacks/spring",
 		}
 		for _, ignoredDir := range ignoredExampleDirs {
 			if strings.Contains(path, ignoredDir) {
