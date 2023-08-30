ef33f52cc0142f8d0a6eb3836727c0d5ca6fa2e1
Savil Srivastava <676452+savil@users.noreply.github.com>
2023-08-30 10:31:06 -0700

    introduce testscript updater

---
+++ testscripts/testrunner/updater/main.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"fmt"
+	"io/fs"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
+	"strings"
+
+	"github.com/pkg/errors"
+	"github.com/samber/lo"
+)
+
+func main() {
+
+	if err := run(); err != nil {
+		panic(err)
+	}
+}
+
+func run() error {
+
+	// loop over all examples that have run_test script
+	// run `devbox update` on each such example
+
+	devboxRepoDir, err := devboxRepoDir()
+	if err != nil {
+		return errors.WithStack(err)
+	}
+	examplesDir := filepath.Join(devboxRepoDir, "examples")
+
+	err = filepath.WalkDir(examplesDir, func(path string, d fs.DirEntry, err error) error {
+		return walkExampleDir(devboxRepoDir, path, d, err)
+	})
+	if err != nil {
+		return errors.WithStack(err)
+	}
+	return nil
+}
+
+var examplesToTry = 0
+
+func walkExampleDir(devboxRepoDir, path string, d fs.DirEntry, err error) error {
+	fmt.Printf("checking %s with name: %s\n", path, d.Name())
+	if err != nil {
+		return errors.WithStack(err)
+	}
+
+	if examplesToTry > 3 {
+		return nil
+	}
+
+	if d.IsDir() {
+		skippedDirs := []string{".devbox", "node_modules"}
+		if lo.Contains(skippedDirs, d.Name()) {
+			return filepath.SkipDir
+		}
+		return nil
+	}
+
+	if d.Name() != "devbox.json" {
+		return nil
+	}
+	contentBytes, err := os.ReadFile(path)
+	if err != nil {
+		return errors.WithStack(err)
+	}
+
+	content := string(contentBytes)
+	if !strings.Contains(content, "run_test") {
+		fmt.Printf("Skipping config at %s\n", path)
+		return nil
+	}
+
+	// run `devbox update` on this example
+	devboxExecutable := filepath.Join(devboxRepoDir, "dist", "devbox")
+	cmd := exec.Command(devboxExecutable, "update", "-c", filepath.Dir(path))
+	if err := cmd.Run(); err != nil {
+		return errors.WithStack(err)
+	}
+	fmt.Printf("Ran `devbox update` on %s\n", path)
+	examplesToTry++
+
+	return nil
+}
+
+func devboxRepoDir() (string, error) {
+	_, filename, _, ok := runtime.Caller(0)
+	if !ok {
+		return "", errors.New("unable to get the current filename")
+	}
+	// This file's directory
+	fileDir := filepath.Dir(filename)
+	// devbox repo directory is 3 levels up
+	return filepath.Join(fileDir, "..", "..", ".."), nil
+}
