e55ef349fd7adb514f8d1971cc4d8f2582910d1a
Mike Landau <mikeland86@gmail.com>
2023-09-22 12:23:21 -0700

    [plugins] Fix init hooks for custom plugins (#1500)
    ## Summary
    
    Ensure custom plugin init hooks run. Previously they were only running
    for builtins.
    
    
    ## How was it tested?
    
    `devbox run run_test`

--- examples/plugins/local/test.sh
+++ examples/plugins/local/test.sh
@@ -4,5 +4,12 @@ if [ -z "$MY_FOO_VAR" ]; then
   echo "MY_FOO_VAR environment variable is not set."
   exit 1
 else
- echo "MY_FOO_VAR is set to '$MY_FOO_VAR'"
+  echo "MY_FOO_VAR is set to '$MY_FOO_VAR'"
+fi
+
+if [ -z "$MY_INIT_HOOK_VAR" ]; then
+  echo "MY_INIT_HOOK_VAR environment variable is not set."
+  exit 1
+else
+  echo "MY_INIT_HOOK_VAR is set to '$MY_INIT_HOOK_VAR'"
 fi
