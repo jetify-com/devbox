87eb31fddd0ac20caeb42dce8d1fd7ae59e13949
Mike Landau <mikeland86@gmail.com>
2023-10-06 11:10:34 -0700

    Remove deprecated field

--- internal/boxcli/shellenv.go
+++ internal/boxcli/shellenv.go
@@ -94,13 +94,14 @@ func shellEnvFunc(cmd *cobra.Command, flags shellEnvCmdFlags) (string, error) {
 }
 
 func shellEnvOnlyPathWithoutWrappersCmd() *cobra.Command {
+	// Deprecated: will be removed after devbox 0.7.0
+	// Don't add deprecated field to avoid printing anything to stdout
 	command := &cobra.Command{
-		Use:        "only-path-without-wrappers",
-		Deprecated: "This command is deprecated and will be removed after devbox 0.7.0",
-		Hidden:     true,
-		Short:      "[internal] Print shell command that exports the system $PATH without the bin-wrappers paths.",
-		Args:       cobra.ExactArgs(0),
-		PreRunE:    ensureNixInstalled,
+		Use:     "only-path-without-wrappers",
+		Hidden:  true,
+		Short:   "[internal] Print shell command that exports the system $PATH without the bin-wrappers paths.",
+		Args:    cobra.ExactArgs(0),
+		PreRunE: ensureNixInstalled,
 		RunE: func(cmd *cobra.Command, args []string) error {
 			s := shellEnvOnlyPathWithoutWrappersFunc()
 			fmt.Fprintln(cmd.OutOrStdout(), s)
