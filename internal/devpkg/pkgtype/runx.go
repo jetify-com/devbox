584fda7c4bd06985ee542d1b1f351703d9254590
Mike Landau <mikeland86@gmail.com>
2023-10-06 11:14:11 -0700

    fmt

--- internal/devpkg/pkgtype/runx.go
+++ internal/devpkg/pkgtype/runx.go
@@ -2,8 +2,10 @@ package pkgtype
 
 import "strings"
 
-const RunXScheme = "runx"
-const RunXPrefix = RunXScheme + ":"
+const (
+	RunXScheme = "runx"
+	RunXPrefix = RunXScheme + ":"
+)
 
 func IsRunX(s string) bool {
 	return strings.HasPrefix(s, RunXPrefix)
