62b320014a8bccc1dc02121ba934db2d0d8f45b4
John Lago <750845+Lagoja@users.noreply.github.com>
2023-11-21 14:00:16 -0800

    simplify example + Hook

--- plugins/pip/venvShellHook.sh
+++ plugins/pip/venvShellHook.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
-if [ -d "$VENV_DIR" ]; then
-    echo "Skipping venv creation, '${VENV_DIR}' already exists"
-else
+if ! [ -d "$VENV_DIR" ]; then
     echo "Creating new venv environment in path: '${VENV_DIR}'"
     python3 -m venv "$VENV_DIR"
 fi
+
 echo "You can activate the virtual environment by running 'source \$VENV_DIR/bin/activate'"
