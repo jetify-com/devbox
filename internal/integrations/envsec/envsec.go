fe3a3edb903c9f97d0969dc3583a44a59906bb03
Mike Landau <mikeland86@gmail.com>
2023-12-20 14:15:43 -0800

    [envsec] Enforce version

--- internal/integrations/envsec/envsec.go
+++ internal/integrations/envsec/envsec.go
@@ -44,7 +44,7 @@ func EnsureInstalled(ctx context.Context) (string, error) {
 		return binPathCache, nil
 	}
 
-	paths, err := pkgtype.RunXClient().Install(ctx, "jetpack-io/envsec")
+	paths, err := pkgtype.RunXClient().Install(ctx, "jetpack-io/envsec@v0.0.11")
 	if err != nil {
 		return "", errors.Wrap(err, "failed to install envsec")
 	}
