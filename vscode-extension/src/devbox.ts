3f955d5c1639305480c8be8bba40050179c9ab96
Mohsen Ansari <mohsenari@users.noreply.github.com>
2024-01-04 23:18:10 +0000

    [vscode-extension] Fixed tab size for extension (#1701)
    ## Summary
    This is just a styling PR to fix indent size across all files in
    extension.
    
    ## How was it tested?
    N/A

--- vscode-extension/src/devbox.ts
+++ vscode-extension/src/devbox.ts
@@ -7,127 +7,124 @@ interface Message {
 }
 
 export async function devboxReopen() {
-
-    if (process.platform === 'win32') {
-        const seeDocs = 'See Devbox docs';
-        const result = await window.showErrorMessage(
-            'This feature is not supported on your platform. \
-            Please open VSCode from inside devbox shell in WSL using the CLI.', seeDocs
-        );
-        if (result === seeDocs) {
-            env.openExternal(Uri.parse('https://www.jetpack.io/devbox/docs/ide_configuration/vscode/#windows-setup'));
-            return;
-        }
-    }
-    await window.withProgress({
-        location: ProgressLocation.Notification,
-        title: "Setting up your Devbox environment. Please don't close vscode.",
-        cancellable: true
-    },
-        async (progress, token) => {
-            token.onCancellationRequested(() => {
-                console.log("User canceled the long running operation");
-            });
-
-            const p = new Promise<void>(async (resolve, reject) => {
-
-                if (workspace.workspaceFolders) {
-                    const workingDir = workspace.workspaceFolders[0].uri;
-                    const dotdevbox = Uri.joinPath(workingDir, '/.devbox');
-                    progress.report({ message: 'Installing devbox packages...', increment: 25 });
-                    await setupDotDevbox(workingDir, dotdevbox);
-
-                    // setup required vscode settings
-                    progress.report({ message: 'Updating configurations...', increment: 50 });
-                    updateVSCodeConf();
-
-                    // Calling CLI to compute devbox env
-                    progress.report({ message: 'Calling Devbox to setup environment...', increment: 80 });
-                    // To use a custom compiled devbox when testing, change this to an absolute path.
-                    const devbox = 'devbox';
-                    // run devbox integrate and then close this window
-                    let child = spawn(devbox, ['integrate', 'vscode'], {
-                        cwd: workingDir.path,
-                        stdio: [0, 1, 2, 'ipc']
-                    });
-                    // if CLI closes before sending "finished" message
-                    child.on('close', (code: number) => {
-                        console.log("child process closed with exit code:", code);
-                        window.showErrorMessage("Failed to setup devbox environment.");
-                        reject();
-                    });
-                    // send config path to CLI
-                    child.send({ configDir: workingDir.path });
-                    // handle CLI finishing the env and sending  "finished"
-                    child.on('message', function (msg: Message, handle) {
-                        if (msg.status === "finished") {
-                            progress.report({ message: 'Finished setting up! Reloading the window...', increment: 100 });
-                            resolve();
-                            commands.executeCommand("workbench.action.closeWindow");
-                        }
-                        else {
-                            console.log(msg);
-                            window.showErrorMessage("Failed to setup devbox environment.");
-                            reject();
-                        }
-                    });
-                }
-            });
-            return p;
-        }
+  if (process.platform === 'win32') {
+    const seeDocs = 'See Devbox docs';
+    const result = await window.showErrorMessage(
+      'This feature is not supported on your platform. \
+      Please open VSCode from inside devbox shell in WSL using the CLI.', seeDocs
     );
+    if (result === seeDocs) {
+      env.openExternal(Uri.parse('https://www.jetpack.io/devbox/docs/ide_configuration/vscode/#windows-setup'));
+      return;
+    }
+  }
+  await window.withProgress({
+    location: ProgressLocation.Notification,
+    title: "Setting up your Devbox environment. Please don't close vscode.",
+    cancellable: true
+  },
+    async (progress, token) => {
+      token.onCancellationRequested(() => {
+        console.log("User canceled the long running operation");
+      });
 
+      const p = new Promise<void>(async (resolve, reject) => {
+        if (workspace.workspaceFolders) {
+          const workingDir = workspace.workspaceFolders[0].uri;
+          const dotdevbox = Uri.joinPath(workingDir, '/.devbox');
+          progress.report({ message: 'Installing devbox packages...', increment: 25 });
+          await setupDotDevbox(workingDir, dotdevbox);
 
+          // setup required vscode settings
+          progress.report({ message: 'Updating configurations...', increment: 50 });
+          updateVSCodeConf();
+
+          // Calling CLI to compute devbox env
+          progress.report({ message: 'Calling Devbox to setup environment...', increment: 80 });
+          // To use a custom compiled devbox when testing, change this to an absolute path.
+          const devbox = 'devbox';
+          // run devbox integrate and then close this window
+          let child = spawn(devbox, ['integrate', 'vscode'], {
+            cwd: workingDir.path,
+            stdio: [0, 1, 2, 'ipc']
+          });
+          // if CLI closes before sending "finished" message
+          child.on('close', (code: number) => {
+            console.log("child process closed with exit code:", code);
+            window.showErrorMessage("Failed to setup devbox environment.");
+            reject();
+          });
+          // send config path to CLI
+          child.send({ configDir: workingDir.path });
+          // handle CLI finishing the env and sending  "finished"
+          child.on('message', function (msg: Message, handle) {
+            if (msg.status === "finished") {
+              progress.report({ message: 'Finished setting up! Reloading the window...', increment: 100 });
+              resolve();
+              commands.executeCommand("workbench.action.closeWindow");
+            }
+            else {
+              console.log(msg);
+              window.showErrorMessage("Failed to setup devbox environment.");
+              reject();
+            }
+          });
+        }
+      });
+      return p;
+    }
+  );
 }
 
 async function setupDotDevbox(workingDir: Uri, dotdevbox: Uri) {
-    try {
-        // check if .devbox exists
-        await workspace.fs.stat(dotdevbox);
-    } catch (error) {
-        //.devbox doesn't exist
-        // running devbox shellenv to create it
-        spawnSync('devbox', ['shellenv'], {
-            cwd: workingDir.path
-        });
-    }
+  try {
+    // check if .devbox exists
+    await workspace.fs.stat(dotdevbox);
+  } catch (error) {
+    //.devbox doesn't exist
+    // running devbox shellenv to create it
+    spawnSync('devbox', ['shellenv'], {
+      cwd: workingDir.path
+    });
+  }
 }
 
 function updateVSCodeConf() {
-    if (process.platform === 'darwin') {
-        const shell = process.env["SHELL"] ?? "/bin/zsh";
-        const shellArgsMap = (shellType: string) => {
-            switch (shellType) {
-                case "fish":
-                    // We special case fish here because fish's `fish_add_path` function
-                    // tends to prepend to PATH by default, hence sourcing the fish config after
-                    // vscode reopens in devbox environment, overwrites devbox packages and 
-                    // might cause confusion for users as to why their system installed packages
-                    // show up when they type for example `which go` as opposed to the packages
-                    // installed by devbox.
-                    return ["--no-config"];
-                default:
-                    return [];
-            }
-        };
-        const shellTypeSlices = shell.split("/");
-        const shellType = shellTypeSlices[shellTypeSlices.length - 1];
-        shellArgsMap(shellType);
-        const devboxCompatibleShell = {
-            "devboxCompatibleShell": {
-                "path": shell,
-                "args": shellArgsMap(shellType)
-            }
-        };
+  if (process.platform === 'darwin') {
+    const shell = process.env["SHELL"] ?? "/bin/zsh";
+    const shellArgsMap = (shellType: string) => {
+      switch (shellType) {
+        case "fish":
+          // We special case fish here because fish's `fish_add_path` function
+          // tends to prepend to PATH by default, hence sourcing the fish config after
+          // vscode reopens in devbox environment, overwrites devbox packages and 
+          // might cause confusion for users as to why their system installed packages
+          // show up when they type for example `which go` as opposed to the packages
+          // installed by devbox.
+          return ["--no-config"];
+        default:
+          return [];
+      }
+    };
+    const shellTypeSlices = shell.split("/");
+    const shellType = shellTypeSlices[shellTypeSlices.length - 1];
+    shellArgsMap(shellType);
+    const devboxCompatibleShell = {
+      "devboxCompatibleShell": {
+        "path": shell,
+        "args": shellArgsMap(shellType)
+      }
+    };
 
-        workspace.getConfiguration().update(
-            'terminal.integrated.profiles.osx',
-            devboxCompatibleShell,
-            ConfigurationTarget.Workspace
-        );
-        workspace.getConfiguration().update(
-            'terminal.integrated.defaultProfile.osx',
-            'devboxCompatibleShell',
-            ConfigurationTarget.Workspace);
-    }
+    workspace.getConfiguration().update(
+      'terminal.integrated.profiles.osx',
+      devboxCompatibleShell,
+      ConfigurationTarget.Workspace
+    );
+    workspace.getConfiguration().update(
+      'terminal.integrated.defaultProfile.osx',
+      'devboxCompatibleShell',
+      ConfigurationTarget.Workspace
+    );
+  }
 }
--- vscode-extension/src/openinvscode.ts
+++ vscode-extension/src/openinvscode.ts
@@ -7,140 +7,139 @@ import { Uri, commands, window } from 'vscode';
 import { chmod, mkdir, open, writeFile } from 'fs/promises';
 
 type VmInfo = {
-    /* eslint-disable @typescript-eslint/naming-convention */
-    vm_id: string;
-    private_key: string;
-    username: string;
-    working_directory: string;
-    /* eslint-enable @typescript-eslint/naming-convention */
+  /* eslint-disable @typescript-eslint/naming-convention */
+  vm_id: string;
+  private_key: string;
+  username: string;
+  working_directory: string;
+  /* eslint-enable @typescript-eslint/naming-convention */
 };
 
 export async function handleOpenInVSCode(uri: Uri) {
-    const queryParams = new URLSearchParams(uri.query);
+  const queryParams = new URLSearchParams(uri.query);
 
-    if (queryParams.has('vm_id') && queryParams.has('token')) {
-        //Not yet supported for windows + WSL - will be added in future
-        if (os.platform() !== 'win32') {
-            window.showInformationMessage('Setting up devbox');
-            // getting ssh keys
-            try {
-                console.debug('Calling getVMInfo...');
-                const response = await getVMInfo(queryParams.get('token'), queryParams.get('vm_id'));
-                const res = await response.json() as VmInfo;
-                console.debug('getVMInfo response: ' + res);
-                // set ssh config
-                console.debug('Calling setupSSHConfig...');
-                await setupSSHConfig(res.vm_id, res.private_key);
+  if (queryParams.has('vm_id') && queryParams.has('token')) {
+    //Not yet supported for windows + WSL - will be added in future
+    if (os.platform() !== 'win32') {
+      window.showInformationMessage('Setting up devbox');
+      // getting ssh keys
+      try {
+        console.debug('Calling getVMInfo...');
+        const response = await getVMInfo(queryParams.get('token'), queryParams.get('vm_id'));
+        const res = await response.json() as VmInfo;
+        console.debug('getVMInfo response: ' + res);
+        // set ssh config
+        console.debug('Calling setupSSHConfig...');
+        await setupSSHConfig(res.vm_id, res.private_key);
 
-                // connect to remote vm
-                console.debug('Calling connectToRemote...');
-                connectToRemote(res.username, res.vm_id, res.working_directory);
-            } catch (err: any) {
-                console.error(err);
-                window.showInformationMessage('Failed to setup devbox remote connection.');
-            }
-        } else {
-            window.showErrorMessage('This function is not yet supported in Windows operating system.');
-        }
+        // connect to remote vm
+        console.debug('Calling connectToRemote...');
+        connectToRemote(res.username, res.vm_id, res.working_directory);
+      } catch (err: any) {
+        console.error(err);
+        window.showInformationMessage('Failed to setup devbox remote connection.');
+      }
     } else {
-        window.showErrorMessage('Error parsing information for remote environment.');
-        console.debug(queryParams.toString());
-    };
+      window.showErrorMessage('This function is not yet supported in Windows operating system.');
+    }
+  } else {
+    window.showErrorMessage('Error parsing information for remote environment.');
+    console.debug(queryParams.toString());
+  };
 }
 
 async function getVMInfo(token: string | null, vmId: string | null): Promise<any> {
-    // send post request to gateway to get vm info and ssh keys
-    const gatewayHost = 'https://api.devbox.sh/g/vm_info';
-    const data = new FormData();
-    data.append("vm_id", vmId);
-    console.debug("calling devbox to get vm_info...");
-    const response = await fetch(gatewayHost, {
-        method: 'post',
-        body: data,
-        headers: {
-            authorization: `Bearer ${token}`
-        }
-    });
-    console.debug("API Call to api.devbox.sh response: " + response);
-    return response;
+  // send post request to gateway to get vm info and ssh keys
+  const gatewayHost = 'https://api.devbox.sh/g/vm_info';
+  const data = new FormData();
+  data.append("vm_id", vmId);
+  console.debug("calling devbox to get vm_info...");
+  const response = await fetch(gatewayHost, {
+    method: 'post',
+    body: data,
+    headers: {
+      authorization: `Bearer ${token}`
+    }
+  });
+  console.debug("API Call to api.devbox.sh response: " + response);
+  return response;
 }
 
 async function setupDevboxLauncher(): Promise<any> {
-    // download devbox launcher script
-    const gatewayHost = 'https://releases.jetpack.io/devbox';
-    const response = await fetch(gatewayHost, {
-        method: 'get',
-    });
-    const launcherPath = `${process.env['HOME']}/.config/devbox/launcher.sh`;
+  // download devbox launcher script
+  const gatewayHost = 'https://releases.jetpack.io/devbox';
+  const response = await fetch(gatewayHost, {
+    method: 'get',
+  });
+  const launcherPath = `${process.env['HOME']}/.config/devbox/launcher.sh`;
 
-    try {
-        const launcherScript = await response.text();
-        const launcherData = new Uint8Array(Buffer.from(launcherScript));
-        const fileHandler = await open(launcherPath, 'w');
-        await writeFile(fileHandler, launcherData, { flag: 'w' });
-        await chmod(launcherPath, 0o711);
-        await fileHandler.close();
-    } catch (err: any) {
-        console.error("error setting up launcher script" + err);
-        throw (err);
-    }
-    return launcherPath;
+  try {
+    const launcherScript = await response.text();
+    const launcherData = new Uint8Array(Buffer.from(launcherScript));
+    const fileHandler = await open(launcherPath, 'w');
+    await writeFile(fileHandler, launcherData, { flag: 'w' });
+    await chmod(launcherPath, 0o711);
+    await fileHandler.close();
+  } catch (err: any) {
+    console.error("error setting up launcher script" + err);
+    throw (err);
+  }
+  return launcherPath;
 }
 
 async function setupSSHConfig(vmId: string, prKey: string) {
-
-    const devboxBinary = await which('devbox', { nothrow: true });
-    let devboxPath = 'devbox';
-    if (devboxBinary === null) {
-        devboxPath = await setupDevboxLauncher();
+  const devboxBinary = await which('devbox', { nothrow: true });
+  let devboxPath = 'devbox';
+  if (devboxBinary === null) {
+    devboxPath = await setupDevboxLauncher();
+  }
+  // For testing change devbox to path to a compiled devbox binary or add --config
+  exec(`${devboxPath} generate ssh-config`, (error, stdout, stderr) => {
+    if (error) {
+      window.showErrorMessage('Failed to setup ssh config. Run VSCode in debug mode to see logs.');
+      console.error(`Failed to setup ssh config: ${error}`);
+      return;
     }
-    // For testing change devbox to path to a compiled devbox binary or add --config
-    exec(`${devboxPath} generate ssh-config`, (error, stdout, stderr) => {
-        if (error) {
-            window.showErrorMessage('Failed to setup ssh config. Run VSCode in debug mode to see logs.');
-            console.error(`Failed to setup ssh config: ${error}`);
-            return;
-        }
-        console.debug(`stdout: ${stdout}`);
-        console.debug(`stderr: ${stderr}`);
-    });
+    console.debug(`stdout: ${stdout}`);
+    console.debug(`stderr: ${stderr}`);
+  });
 
-    // save private key to file
-    const prkeyDir = `${process.env['HOME']}/.config/devbox/ssh/keys`;
-    await ensureDir(prkeyDir);
-    const prkeyPath = `${prkeyDir}/${vmId}.vm.devbox-vms.internal`;
-    try {
-        const prKeydata = new Uint8Array(Buffer.from(prKey));
-        const fileHandler = await open(prkeyPath, 'w');
-        await writeFile(fileHandler, prKeydata, { flag: 'w' });
-        await chmod(prkeyPath, 0o600);
-        await fileHandler.close();
-    } catch (err: any) {
-        // When a request is aborted - err is an AbortError
-        console.error('Failed to setup ssh config: ' + err);
-        throw (err);
-    }
+  // save private key to file
+  const prkeyDir = `${process.env['HOME']}/.config/devbox/ssh/keys`;
+  await ensureDir(prkeyDir);
+  const prkeyPath = `${prkeyDir}/${vmId}.vm.devbox-vms.internal`;
+  try {
+    const prKeydata = new Uint8Array(Buffer.from(prKey));
+    const fileHandler = await open(prkeyPath, 'w');
+    await writeFile(fileHandler, prKeydata, { flag: 'w' });
+    await chmod(prkeyPath, 0o600);
+    await fileHandler.close();
+  } catch (err: any) {
+    // When a request is aborted - err is an AbortError
+    console.error('Failed to setup ssh config: ' + err);
+    throw (err);
+  }
 }
 
 function connectToRemote(username: string, vmId: string, workDir: string) {
-    try {
-        const host = `${username}@${vmId}.vm.devbox-vms.internal`;
-        const workspaceURI = `vscode-remote://ssh-remote+${host}${workDir}`;
-        const uriToOpen = Uri.parse(workspaceURI);
-        console.debug("uriToOpen: ", uriToOpen.toString());
-        commands.executeCommand("vscode.openFolder", uriToOpen, false);
-    } catch (err: any) {
-        console.error('failed to connect to remote: ' + err);
-    }
+  try {
+    const host = `${username}@${vmId}.vm.devbox-vms.internal`;
+    const workspaceURI = `vscode-remote://ssh-remote+${host}${workDir}`;
+    const uriToOpen = Uri.parse(workspaceURI);
+    console.debug("uriToOpen: ", uriToOpen.toString());
+    commands.executeCommand("vscode.openFolder", uriToOpen, false);
+  } catch (err: any) {
+    console.error('failed to connect to remote: ' + err);
+  }
 }
 
 async function ensureDir(dir: string) {
-    try {
-        await mkdir(dir, {recursive: true, mode: 0o700});
-    } catch (err: any) {
-        if (err.code !== 'EEXIST') {
-            console.error('Failed to setup ssh keys directory: ' + err);
-            throw (err);
-        }
+  try {
+    await mkdir(dir, {recursive: true, mode: 0o700});
+  } catch (err: any) {
+    if (err.code !== 'EEXIST') {
+      console.error('Failed to setup ssh keys directory: ' + err);
+      throw (err);
     }
+  }
 }
