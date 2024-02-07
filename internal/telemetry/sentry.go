1844c8f60f2f01284b514a53862175d3a07821b2
Mike Landau <mikeland86@gmail.com>
2024-02-07 15:51:04 -0800

    Rename release

--- internal/telemetry/sentry.go
+++ internal/telemetry/sentry.go
@@ -39,7 +39,7 @@ func initSentryClient(appName string) bool {
 	err := sentry.Init(sentry.ClientOptions{
 		Dsn:              build.SentryDSN,
 		Environment:      environment,
-		Release:          build.Version,
+		Release:          appName + "@" + build.Version,
 		Transport:        transport,
 		TracesSampleRate: 1,
 		BeforeSend: func(event *sentry.Event, _ *sentry.EventHint) *sentry.Event {
