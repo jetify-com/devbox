e6f9841ead24ba89d5dcd4f72d2a0a0b5768d75d
Mike Landau <mikeland86@gmail.com>
2024-02-22 17:26:42 -0800

    Fix indent

--- examples/stacks/lapp-stack/my_app/config.php
+++ examples/stacks/lapp-stack/my_app/config.php
@@ -1,6 +1,6 @@
 <?php
-  $db_hostname  = "127.0.0.1";
-  $db_port 	    = getenv("PGPORT");
-  $db_database	= "devbox_lapp";
-  $db_username	= "devbox_user";
-  $db_password	= "password";
+  $db_hostname = "127.0.0.1";
+  $db_port     = getenv("PGPORT");
+  $db_database = "devbox_lapp";
+  $db_username = "devbox_user";
+  $db_password = "password";
