580bd133239dc35359bfa7b5be935f9dce607654
Lucille Hua <lucille.hua@gmail.com>
2024-04-09 12:30:42 -0400

    add period

--- internal/nix/nixstore/fs.go
+++ internal/nix/nixstore/fs.go
@@ -1,4 +1,4 @@
-// Copyright 2024 Jetify Inc and contributors. All rights reserved.
+// Copyright 2024 Jetify Inc. and contributors. All rights reserved.
 // Use of this source code is governed by the license in the LICENSE file.
 
 package nixstore
