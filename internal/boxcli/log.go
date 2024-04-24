18eca386bcef50b9372fec23f0192ea9f05df0da
Mike Landau <mikeland86@gmail.com>
2024-04-24 11:53:31 -0700

    [telemetry] Add uid, org_id, and nix build event (#2018)
    ## Summary
    
    This PR improves segment logging by:
    
    * Adding orgID if user is logged in.
    * Using userID if user is logged in.
    * Removing anonymousID if userID is non zero. (Segment will remove
    userID if we pass an anonymousID)
    * Add new event to track `nix build`
    
    This allows us to:
    
    * Use orgID to segment data, potentially allowing individual orgs to
    export/use their own data as needed.
    * Track logged in users.
    * Track which packages are slow to build.
    
    Private Notes:
    
    * We still respect `DO_NOT_TRACK` even if user is logged in. 
    * userID and orgID data is only sent if user is logged in.
    
    cc: @Lagoja 
    
    ## How was it tested?
    
    Logged to development segment/amplitude, built a quick table showing nix
    average build times:
    
    <img width="1208" alt="image"
    src="https://github.com/jetify-com/devbox/assets/544948/4ec2d49e-c886-4032-a5c6-9dff6986d780">

--- internal/boxcli/log.go
+++ internal/boxcli/log.go
@@ -33,14 +33,14 @@ func doLogCommand(cmd *cobra.Command, args []string) error {
 			return usererr.New("expected a start-time argument for logging the shell-ready event")
 		}
 		telemetry.Event(telemetry.EventShellReady, telemetry.Metadata{
-			CommandStart: telemetry.ParseShellStart(args[1]),
+			EventStart: telemetry.ParseShellStart(args[1]),
 		})
 	case "shell-interactive":
 		if len(args) < 2 {
 			return usererr.New("expected a start-time argument for logging the shell-interactive event")
 		}
 		telemetry.Event(telemetry.EventShellInteractive, telemetry.Metadata{
-			CommandStart: telemetry.ParseShellStart(args[1]),
+			EventStart: telemetry.ParseShellStart(args[1]),
 		})
 	}
 	return usererr.New("unrecognized event-name %s for command: %s", args[0], cmd.CommandPath())
