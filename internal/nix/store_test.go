0ae7ea8c2ba7eab107c5d6032a9ca3d4a02342a6
Mike Landau <mikeland86@gmail.com>
2024-05-22 18:04:08 -0700

    Fix tests

--- internal/nix/store_test.go
+++ internal/nix/store_test.go
@@ -3,6 +3,8 @@ package nix
 import (
 	"slices"
 	"testing"
+
+	"golang.org/x/exp/maps"
 )
 
 func TestParseStorePathFromInstallableOutput(t *testing.T) {
@@ -19,18 +21,18 @@ func TestParseStorePathFromInstallableOutput(t *testing.T) {
 		},
 		{
 			name:     "go-basic-nix-2-17-0",
-			input:    `[{"path":"/nix/store/fgkl3qk8p5hnd07b0dhzfky3ys5gxjmq-go-1.22.0","valid":false}]`,
+			input:    `[{"path":"/nix/store/fgkl3qk8p5hnd07b0dhzfky3ys5gxjmq-go-1.22.0","valid":true}]`,
 			expected: []string{"/nix/store/fgkl3qk8p5hnd07b0dhzfky3ys5gxjmq-go-1.22.0"},
 		},
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			actual, err := parseStorePathFromInstallableOutput(tc.name, []byte(tc.input))
+			actual, err := parseStorePathFromInstallableOutput([]byte(tc.input))
 			if err != nil {
 				t.Errorf("Expected no error but got error: %s", err)
 			}
-			if !slices.Equal(tc.expected, actual) {
+			if !slices.Equal(tc.expected, maps.Keys(actual)) {
 				t.Errorf("Expected store path %s but got %s", tc.expected, actual)
 			}
 		})
