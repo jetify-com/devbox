43ad4579c42f3c685dd1f2ed63b8250b4c9afd5c
John Lago <750845+Lagoja@users.noreply.github.com>
2024-05-30 16:42:51 -0700

    Only notify users about Venv Activation on setup

--- plugins/pip/venvShellHook.sh
+++ plugins/pip/venvShellHook.sh
@@ -3,6 +3,6 @@
 if ! [ -d "$VENV_DIR" ]; then
     echo "Creating new venv environment in path: '${VENV_DIR}'"
     python3 -m venv "$VENV_DIR"
+    echo "You can activate the virtual environment by running '. \$VENV_DIR/bin/activate' (for fish shell, replace '.' with 'source')" >&2
 fi
 
-echo "You can activate the virtual environment by running '. \$VENV_DIR/bin/activate' (for fish shell, replace '.' with 'source')" >&2
