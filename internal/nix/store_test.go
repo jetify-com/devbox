b95f4cd2aa010a7a8a516b2c706cbf494313c0fe
Mike Landau <mikeland86@gmail.com>
2024-05-30 12:09:05 -0700

    [nix] Fix StorePathsAreInStore (#2104)
    ## Summary
    
    Addresses issue introduced in 
    
    https://github.com/jetify-com/devbox/pull/2076
    and
    https://github.com/jetify-com/devbox/pull/2098
    
    Basically we have 2 different uses cases when looking up store paths.
    Sometimes we want all store paths (whether installed or not) and in
    other cases we only want paths that are in local store. This change
    fixes that and adds some testing.
    
    ## How was it tested?
    
    unit tests, manual integration test by installing a package that was not
    in local store.

--- internal/nix/store_test.go
+++ internal/nix/store_test.go
@@ -1,7 +1,6 @@
 package nix
 
 import (
-	"slices"
 	"testing"
 
 	"golang.org/x/exp/maps"
@@ -11,18 +10,37 @@ func TestParseStorePathFromInstallableOutput(t *testing.T) {
 	testCases := []struct {
 		name     string
 		input    string
-		expected []string
+		expected map[string]bool
 	}{
 		{
 			name: "go-basic-nix-2-20-1",
 			// snipped the actual output for brevity. We mainly care about the first key in the JSON.
-			input:    `{"/nix/store/fgkl3qk8p5hnd07b0dhzfky3ys5gxjmq-go-1.22.0":{"deriver":"/nix/store/clr3bm8njqysvyw4r4x4xmldhz4knrff-go-1.22.0.drv"}}`,
-			expected: []string{"/nix/store/fgkl3qk8p5hnd07b0dhzfky3ys5gxjmq-go-1.22.0"},
+			input: `{"/nix/store/fgkl3qk8p5hnd07b0dhzfky3ys5gxjmq-go-1.22.0":{"deriver":"/nix/store/clr3bm8njqysvyw4r4x4xmldhz4knrff-go-1.22.0.drv"}}`,
+			expected: map[string]bool{
+				"/nix/store/fgkl3qk8p5hnd07b0dhzfky3ys5gxjmq-go-1.22.0": true,
+			},
 		},
 		{
-			name:     "go-basic-nix-2-17-0",
-			input:    `[{"path":"/nix/store/fgkl3qk8p5hnd07b0dhzfky3ys5gxjmq-go-1.22.0"}]`,
-			expected: []string{"/nix/store/fgkl3qk8p5hnd07b0dhzfky3ys5gxjmq-go-1.22.0"},
+			name: "go-basic-nix-2-20-1",
+			// snipped the actual output for brevity. We mainly care about the first key in the JSON.
+			input: `{"/nix/store/fgkl3qk8p5hnd07b0dhzfky3ys5gxjmq-go-1.22.0":null}`,
+			expected: map[string]bool{
+				"/nix/store/fgkl3qk8p5hnd07b0dhzfky3ys5gxjmq-go-1.22.0": false,
+			},
+		},
+		{
+			name:  "go-basic-nix-2-17-0",
+			input: `[{"path":"/nix/store/fgkl3qk8p5hnd07b0dhzfky3ys5gxjmq-go-1.22.0"}]`,
+			expected: map[string]bool{
+				"/nix/store/fgkl3qk8p5hnd07b0dhzfky3ys5gxjmq-go-1.22.0": false,
+			},
+		},
+		{
+			name:  "go-basic-nix-2-17-0",
+			input: `[{"path":"/nix/store/fgkl3qk8p5hnd07b0dhzfky3ys5gxjmq-go-1.22.0", "valid": true}]`,
+			expected: map[string]bool{
+				"/nix/store/fgkl3qk8p5hnd07b0dhzfky3ys5gxjmq-go-1.22.0": true,
+			},
 		},
 	}
 
@@ -32,8 +50,8 @@ func TestParseStorePathFromInstallableOutput(t *testing.T) {
 			if err != nil {
 				t.Errorf("Expected no error but got error: %s", err)
 			}
-			if !slices.Equal(tc.expected, maps.Keys(actual)) {
-				t.Errorf("Expected store path %s but got %s", tc.expected, actual)
+			if !maps.Equal(tc.expected, actual) {
+				t.Errorf("Expected store path %v but got %v", tc.expected, actual)
 			}
 		})
 	}
