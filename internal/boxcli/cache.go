bad5e708256b6dfb6f66799169f995f6568596d2
Greg Curtis <greg.curtis@jetpack.io>
2024-05-31 10:48:34 -0400

    heredoc.Doc

--- internal/boxcli/cache.go
+++ internal/boxcli/cache.go
@@ -82,19 +82,21 @@ func cacheConfigureCmd() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "configure",
 		Short: "Configure Nix to use the Devbox cache as a substituter",
-		Long: "Configure Nix to use the Devbox cache as a substituter.\n" +
-			"\n" +
-			"If the current Nix installation is multi-user, this command grants the Nix\n" +
-			"daemon access to Devbox caches by making the following changes:\n" +
-			"\n" +
-			"- Adds the current user to Nix's list of trusted users in the system nix.conf.\n" +
-			"- Adds the cache credentials to ~root/.aws/config.\n" +
-			"\n" +
-			"Configuration requires sudo, but only needs to happen once. The changes persist\n" +
-			"across Devbox accounts and organizations.\n" +
-			"\n" +
-			"This command is a no-op for single-user Nix installs that aren't running the\n" +
-			"Nix daemon.",
+		Long: heredoc.Doc(`
+			Configure Nix to use the Devbox cache as a substituter.
+
+			If the current Nix installation is multi-user, this command grants the Nix
+			daemon access to Devbox caches by making the following changes:
+
+			- Adds the current user to Nix's list of trusted users in the system nix.conf.
+			- Adds the cache credentials to ~root/.aws/config.
+
+			Configuration requires sudo, but only needs to happen once. The changes persist
+			across Devbox accounts and organizations.
+
+			This command is a no-op for single-user Nix installs that aren't running the
+			Nix daemon.
+		`),
 		Hidden: true,
 		Args:   cobra.MaximumNArgs(0),
 		RunE: func(cmd *cobra.Command, args []string) error {
