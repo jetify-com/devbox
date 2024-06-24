b01129f6aa4007130aca3f26bf7fca7a3cb37dd8
Mike Landau <mikeland86@gmail.com>
2024-06-24 11:47:07 -0700

    [lock-tidy] Put warning behind feature flag

---
+++ internal/boxcli/featureflag/tidywarning.go
@@ -0,0 +1,6 @@
+// Copyright 2024 Jetify Inc. and contributors. All rights reserved.
+// Use of this source code is governed by the license in the LICENSE file.
+
+package featureflag
+
+var TidyWarning = disable("TIDY_WARNING")
