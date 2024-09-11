6df893aad7550c191d876f6edb94df93a61d2adb
John Lago <750845+Lagoja@users.noreply.github.com>
2024-09-11 10:23:54 -0700

    [Python] Change `.venv` script to be more compatible with IDEs (#2259)
    ## Summary
    
    Moves `.venv` to the Devbox Project Root, and adds some checks to ensure
    we warn the user if we are going to squash a user created venv. Moving
    `.venv` to the project root makes it more likely that Python Extensions
    and IDEs will pick up the devbox managed python, instead of a system
    python
    
    Fixes DEV-105
    
    ## How was it tested?
    
    ---------
    
    Co-authored-by: John Lago <>

--- plugins/pip/venvShellHook.sh
+++ plugins/pip/venvShellHook.sh
@@ -1,8 +1,54 @@
 #!/bin/sh
+set -eu
+STATE_FILE="$DEVBOX_PROJECT_ROOT/.devbox/venv_check_completed"
 
-if ! [ -d "$VENV_DIR" ]; then
-    echo "Creating new venv environment in path: '${VENV_DIR}'"
-    python3 -m venv "$VENV_DIR"
-    echo "You can activate the virtual environment by running '. \$VENV_DIR/bin/activate' (for fish shell, replace '.' with 'source')" >&2
+is_valid_venv() {
+    [ -f "$1/bin/activate" ] && [ -f "$1/bin/python" ]
+}
+
+is_devbox_venv() {
+    [ "$1/bin/python" -ef "$DEVBOX_PACKAGES_DIR/bin/python" ]
+}
+
+create_venv() {
+    python -m venv "$VENV_DIR" --clear
+    echo "*\n.*" >> "$VENV_DIR/.gitignore"
+}
+
+# Check if we've already run this script
+if [ -f "$STATE_FILE" ]; then
+    # "We've already run this script. Exiting..."
+    exit 0
 fi
 
+# Check that Python version supports venv
+if ! python -c 'import venv' 1> /dev/null 2> /dev/null; then
+    echo "\033[1;33mWARNING: Python version must be > 3.3 to create a virtual environment.\033[0m"
+    touch "$STATE_FILE"
+    exit 1
+fi
+
+# Check if the directory exists
+if [ -d "$VENV_DIR" ]; then
+    if is_valid_venv "$VENV_DIR"; then
+        if ! is_devbox_venv "$VENV_DIR"; then
+            echo "\033[1;33mWARNING: Virtual environment at $VENV_DIR doesn't use Devbox Python.\033[0m"
+            read -p "Do you want to overwrite it? (y/n) " -n 1 -r
+            echo
+            if [[ $REPLY =~ ^[Yy]$ ]]; then
+                echo "Overwriting existing virtual environment..."
+                create_venv
+            else
+                echo "Using your existing virtual environment. We recommend changing \$VENV_DIR to a different location"
+                touch "$STATE_FILE"
+                exit 1
+            fi
+        fi
+    else
+        echo "Directory exists but is not a valid virtual environment. Creating a new one..."
+        create_venv
+    fi
+else
+    echo "Virtual environment directory doesn't exist. Creating new one..."
+    create_venv
+fi
