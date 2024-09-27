37c36a1b8811d23608ef72693172b6be43b84310
John Lago <750845+Lagoja@users.noreply.github.com>
2024-09-27 16:51:49 -0700

    Build and Install Devbox via Nix Flake (#2308)
    ## Summary
    
    This adds a flake.nix to our repo for building and distributing Devbox.
    This will make it easier for developers who install Devbox with Nix to
    use pre-releases, and keep Devbox up-to-date on their system.
    
    This changes our workflow in a few small ways:
    
    1. Whenever `go.mod` changes, we'll need to run `devbox run update-hash`
    to update the vendorHash for the flake. You can also run this script
    when you run `go mod tidy`
    2. Releasing a new version will also require us to update the version
    number in the flake.nix. This should be done with a commit ("Bump
    version to x.y") that also gets tagged with the release number.
    3. For debugging purposes, the flake will add the shortened commit hash
    to the version string. This also helps identify if a user built the
    binary from a flake.
    4. I've added a test to `cli-tests` that will build the flake. I've
    confirmed that it will fail if the vendorHash is out of date or
    incorrect
    5. We'll periodically need to run `nix flake update` to update the
    flake.lock file.
    
    ## How was it tested?
    
    1. Ran `nix build .` in the Devbox repo, confirmed that the resulting
    build worked
    2. Tested the Github Action by pushing an invalid vendorHash
    
    ---------
    
    Signed-off-by: John Lago <750845+Lagoja@users.noreply.github.com>
    Co-authored-by: Greg Curtis <greg.curtis@jetpack.io>

---
+++ flake.nix
@@ -0,0 +1,69 @@
+{
+  description = "Instant, easy, predictable shells and containers";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        lastTag = "0.13.2";
+
+        # Add the commit to the version string, in case someone builds from main
+        getVersion = pkgs.lib.trivial.pipe self [
+          (x: "${lastTag}")
+          (x: if (self ? revCount)
+              then "${x}-${self.shortRev}"
+              else "${x}-${self.dirtyShortRev}")
+        ];
+
+        # Run `devbox run update-flake` to update the vendorHash
+        vendorHash = if builtins.pathExists ./vendor-hash
+                     then builtins.readFile ./vendor-hash
+                     else "";
+
+        buildGoModule = pkgs.buildGo123Module;
+
+      in
+      {
+        inherit self;
+        packages.default = buildGoModule rec {
+          pname = "devbox";
+          version = getVersion;
+
+          src = ./.;
+
+          inherit vendorHash;
+
+          ldflags = [
+            "-s"
+            "-w"
+            "-X go.jetpack.io/devbox/internal/build.Version=${version}"
+          ];
+
+          # Disable tests if they require network access or are integration tests
+          doCheck = false;
+
+          nativeBuildInputs = [ pkgs.installShellFiles ];
+
+          postInstall = ''
+            installShellCompletion --cmd devbox \
+              --bash <($out/bin/devbox completion bash) \
+              --fish <($out/bin/devbox completion fish) \
+              --zsh <($out/bin/devbox completion zsh)
+          '';
+
+          meta = with pkgs.lib; {
+            description = "Instant, easy, and predictable development environments";
+            homepage = "https://www.jetify.com/devbox";
+            license = licenses.asl20;
+            maintainers = with maintainers; [ lagoja ];
+          };
+        };
+      }
+    );
+}
