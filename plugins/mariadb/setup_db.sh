f8b26f6450fd820afa13806671f4dc98fd58f618
John Lago <750845+Lagoja@users.noreply.github.com>
2025-01-02 13:48:52 -0800

    Add Fix MariaDB/MySQL detection, add my.cnf to mariadb

--- plugins/mariadb/setup_db.sh
+++ plugins/mariadb/setup_db.sh
@@ -6,3 +6,7 @@ if [ ! -d "$MYSQL_DATADIR" ]; then
         --datadir=$MYSQL_DATADIR --basedir=$MYSQL_BASEDIR \
         --pid-file=$MYSQL_PID_FILE
 fi
+
+if [ -e "$MYSQL_CONF" ]; then
+  ln -fs "$MYSQL_CONF" "$MYSQL_HOME/my.cnf"
+fi
