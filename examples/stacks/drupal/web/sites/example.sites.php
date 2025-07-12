975f73c5789abb13d0776a35d5795b56dd81d185
Daniel Loreto <279789+loreto@users.noreply.github.com>
2025-07-12 11:02:00 -0500

    Upgrade some dependencies

--- examples/stacks/drupal/web/sites/example.sites.php
+++ examples/stacks/drupal/web/sites/example.sites.php
@@ -7,7 +7,7 @@
  * Configuration file for multi-site support and directory aliasing feature.
  *
  * This file is required for multi-site support and also allows you to define a
- * set of aliases that map hostnames, ports, and pathnames to configuration
+ * set of aliases that map host names, ports, and path names to configuration
  * directories in the sites directory. These aliases are loaded prior to
  * scanning for directories, and they are exempt from the normal discovery
  * rules. See default.settings.php to view how Drupal discovers the
@@ -53,5 +53,5 @@
  *
  * @see default.settings.php
  * @see \Drupal\Core\DrupalKernel::getSitePath()
- * @see https://www.drupal.org/documentation/install/multi-site
+ * @see https://www.drupal.org/docs/getting-started/multisite-drupal
  */
