fcd0d973c029450dabb526e819f961e84e2e8c24
tlelson <tpj800@gmail.com>
2025-09-02 11:05:56 +1000

    Update poetry demo due to major version change
    Poetry is now on major version 2 and has had some significant changes.
    
    The `poetry shell` command is deprecated and `[tool.poetry.scripts]` no
    longer are available on the path.
    
    This commit updates updates the docs and the initHook to ensure that
    scripts are available.
    
    It makes the compromise of using poetry to manage the installed python
    version instead of devbox
    
    Issue: [Poetry shell missing scripts](https://github.com/jetify-com/devbox/issues/2676)

--- examples/development/python/poetry/poetry-demo/poetry_demo/__main__.py
+++ examples/development/python/poetry/poetry-demo/poetry_demo/__main__.py
@@ -1,4 +1,14 @@
 from emoji import emojize
 
-if __name__ == "__main__":
+
+def main():
+    """Prints a greeting message.
+
+    This is a separate function so that it can be used as a script in the pyproject.toml
+    """
     print(emojize(":rocket: Devbox with Poetry :rocket:"))
+
+
+if __name__ == "__main__":
+    main()
+
