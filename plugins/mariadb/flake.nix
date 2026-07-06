48ef009aa69114551fc2c93d0cd40dc1791799cd
Jeff Turner <jeff@redradishtech.com>
2026-07-06 18:01:14 +1000

    fix(mariadb,mysql plugin): ignore /etc/mysql config files, for better isolation (#2904)
    By default, mariadbd reads /etc/mysql/mariadb.cnf, and if the system has
    another version of MariaDB installed, those config files can cause the
    Devbox mariadbd to fail (e.g. requesting loading plugins not available).
    
    This commit causes devbox mariadbd (and mysqld) to only read from
    $MYSQL_CONF (typically devbox.d/mariadb/my.cnf).
    
    Additionally, wrap the client/admin binaries (mariadb, mariadb-admin,
    mariadb-dump, mysqldump, mysql, mysqladmin) with --defaults-file and
    --socket so they also ignore /etc/mysql and connect to the correct
    socket.
    
    Fixes #2904

--- plugins/mariadb/flake.nix
+++ plugins/mariadb/flake.nix
@@ -14,28 +14,37 @@
         nativeBuildInputs = [ nixpkgs.legacyPackages.{{.System}}.makeWrapper];
         postBuild = ''
 
-          wrapProgram $out/bin/mysqld \
-            --add-flags '--basedir=$out --datadir=''$MYSQL_DATADIR --pid-file=''$MYSQL_PID_FILE --socket=''$MYSQL_UNIX_PORT';
-
           wrapProgram $out/bin/mariadbd \
-            --add-flags '--basedir=$out --datadir=''$MYSQL_DATADIR --pid-file=''$MYSQL_PID_FILE --socket=''$MYSQL_UNIX_PORT';
-
-          wrapProgram $out/bin/mysqld_safe \
-            --add-flags '--basedir=$out --datadir=''$MYSQL_DATADIR --pid-file=''$MYSQL_PID_FILE --socket=''$MYSQL_UNIX_PORT';
+            --add-flags '--defaults-file=''$MYSQL_CONF --basedir=$out --datadir=''$MYSQL_DATADIR --pid-file=''$MYSQL_PID_FILE --socket=''$MYSQL_UNIX_PORT';
 
           if [ -f $out/bin/mariadbd-safe ]; then
             wrapProgram $out/bin/mariadbd-safe \
-              --add-flags '--basedir=$out --datadir=''$MYSQL_DATADIR --pid-file=''$MYSQL_PID_FILE --socket=''$MYSQL_UNIX_PORT';
+              --add-flags '--defaults-file=''$MYSQL_CONF --basedir=$out --datadir=''$MYSQL_DATADIR --pid-file=''$MYSQL_PID_FILE --socket=''$MYSQL_UNIX_PORT';
           fi
 
-          wrapProgram "$out/bin/mysql_install_db" \
-            --add-flags '--basedir=$out --datadir=''$MYSQL_DATADIR --pid-file=''$MYSQL_PID_FILE --basedir=''$MYSQL_BASEDIR';
+          # my_print_defaults needs --defaults-file too, otherwise it
+          # falls back to /etc/mysql. mariadb-install-db calls it via
+          # $print_defaults, not via mariadbd's wrapper.
+          wrapProgram $out/bin/my_print_defaults \
+            --add-flags '--defaults-file=''$MYSQL_CONF';
 
           if [ -f $out/bin/mariadb-install-db ]; then
             # Note: no --defaults-file= as mariadb-install-db wraps mariadbd which already has it
             wrapProgram "$out/bin/mariadb-install-db" \
               --add-flags '--basedir=$out --datadir=''$MYSQL_DATADIR --pid-file=''$MYSQL_PID_FILE --basedir=''$MYSQL_BASEDIR';
           fi
+
+          wrapProgram $out/bin/mariadb-admin \
+            --add-flags '--defaults-file=''$MYSQL_CONF --socket=''$MYSQL_UNIX_PORT';
+
+          wrapProgram $out/bin/mariadb \
+            --add-flags '--defaults-file=''$MYSQL_CONF --socket=''$MYSQL_UNIX_PORT';
+
+          wrapProgram $out/bin/mariadb-dump \
+            --add-flags '--defaults-file=''$MYSQL_CONF --socket=''$MYSQL_UNIX_PORT';
+
+          # Don't wrap 'mysql' or any mysql binaries, as they are correctly left as symlinks
+
         '';
       };
     in{
