ada57b946936330cac2681bae7b86a530fc75d77
Jeff Turner <jeff@redradishtech.com>
2026-07-30 17:10:23 +1000

    fix(mariadb plugin): mariadbd-safe and mariadb-install-db had no devbox wrapper
    The intention of mariadb/flake.nix is to invoke these commands with Devbox-specific flags:
    
    mariadb-safe --defaults-file=$MYSQL_CONF --basedir=$out --datadir=$MYSQL_DATADIR --pid-file=$MYSQL_PID_FILE
    mariadb-install-db --basedir=$out --datadir=$MYSQL_DATADIR --pid-file=$MYSQL_PID_FILE --basedir=$MYSQL_BASEDIR
    
    but that was not happening - the upstream Nix versions were being used with no extra flags.
    
    Notably this caused mariadb-install-db to inherit settings from /etc/mysql. If e.g.
    /etc/mysql/mariadb.conf.d/50-server.cnf sets 'user=mysql', mariadb-install-db fails.
    
    A second, hidden-till-now bug is that flake.nix wrapped `mariadb_install_db` and `mariadbd_safe`, when the correct
    upstream scripts are `mariadb-install-db` and `mariadbd-safe`.

--- plugins/mariadb/flake.nix
+++ plugins/mariadb/flake.nix
@@ -23,16 +23,17 @@
           wrapProgram $out/bin/mysqld_safe \
             --add-flags '--basedir=$out --datadir=''$MYSQL_DATADIR --pid-file=''$MYSQL_PID_FILE --socket=''$MYSQL_UNIX_PORT';
 
-          if [-f $out/bin/mariadbd-safe]; then
-            wrapProgram $out/bin/mariadbd_safe \
+          if [ -f $out/bin/mariadbd-safe ]; then
+            wrapProgram $out/bin/mariadbd-safe \
               --add-flags '--basedir=$out --datadir=''$MYSQL_DATADIR --pid-file=''$MYSQL_PID_FILE --socket=''$MYSQL_UNIX_PORT';
           fi
 
           wrapProgram "$out/bin/mysql_install_db" \
             --add-flags '--basedir=$out --datadir=''$MYSQL_DATADIR --pid-file=''$MYSQL_PID_FILE --basedir=''$MYSQL_BASEDIR';
 
-          if [-f $out/bin/mariadb-install-db]; then
-            wrapProgram "$out/bin/mariadb_install_db" \
+          if [ -f $out/bin/mariadb-install-db ]; then
+            # Note: no --defaults-file= as mariadb-install-db wraps mariadbd which already has it
+            wrapProgram "$out/bin/mariadb-install-db" \
               --add-flags '--basedir=$out --datadir=''$MYSQL_DATADIR --pid-file=''$MYSQL_PID_FILE --basedir=''$MYSQL_BASEDIR';
           fi
         '';
